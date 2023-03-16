91eb31cff47018e73fc90ab131ad9867d37a092b
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-16 14:08:04 +0900

    dd

--- MH/app.py
+++ MH/app.py
@@ -3,16 +3,17 @@ import numpy as np
 
 tab0, tab1, tab2 = st.tabs(["🏠 Homepage", "📈 Chart", "🗃 Data"])
 data = np.random.randn(10, 1)
+with tab0:
+    tab0.subheader("2030의 소비트렌드 분석")
+    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+    '''
+    * ~홈페이지~
+    * 안뇽
 
-tab0.subheader("2030의 소비트렌드 분석")
-st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-'''
-* ~홈페이지~
-* 안뇽
-
-'''
-tab1.subheader("A tab with a chart")
-tab1.line_chart(data)
-
-tab2.subheader("A tab with the data")
-tab2.write(data)
+    '''
+with tab1:
+    tab1.subheader("A tab with a chart")
+    tab1.line_chart(data)
+with tab2:
+    tab2.subheader("A tab with the data")
+    tab2.write(data)
