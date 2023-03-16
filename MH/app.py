1ffa7d2000eab0655134a001731ec57ac739185b
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-16 12:06:50 +0900

    ㅇ

--- MH/app.py
+++ MH/app.py
@@ -5,23 +5,16 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from google.colab import files
 
+import streamlit as st
+import pandas as pd
+
 # 엑셀 파일 불러오기
-uploaded_file = st.file_uploader("KDX2021_SSC_ONLINE_DATA", type="xlsx")
+uploaded_file = st.file_uploader("Choose an Excel file", type="xlsx")
 
 if uploaded_file is not None:
     df = pd.read_excel(uploaded_file)
     st.write(df)
-    
 
-#st.write 내부만 고치기!
-
-st.write(
-    """
-   
-# streamlit에 pie chart 업로드
-st.pyplot(fig)
-
-    """
 )
 # https://pixabay.com/ko
 st.image(
