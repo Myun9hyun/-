25444ebb0c456206074a2436501e20533f02f2dc
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-16 14:13:17 +0900

    ㅇㅇ

--- MH/app.py
+++ MH/app.py
@@ -5,6 +5,7 @@ tab0, tab1, tab2 = st.tabs(["🏠 Homepage", "📈 Chart", "🗃 Data"])
 data = np.random.randn(10, 1)
 with tab0:
     tab0.subheader("2030의 소비트렌드 분석")
+    st.write("위의 탭에 있는 메뉴를 클릭해 선택하신 항목을 볼 수 있습니다.")
     st.image("https://cdn.pixabay.com/photo/2018/01/07/20/56/graph-3068300_960_720.jpg", width=500)
     '''
     * ~홈페이지~
