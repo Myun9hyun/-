7875ae8c853ad07232e29d5a833b7f4ddd84696e
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-16 12:43:13 +0900

    ㅇ

--- MH/app.py
+++ MH/app.py
@@ -1,28 +1,9 @@
-pip install plotly
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 
+# 엑셀 파일 불러오기
+uploaded_file = st.file_uploader("Choose an Excel file", type="xlsx")
 
-df = pd.DataFrame({
-    'Option': ['Option 1', 'Option 2', 'Option 3'],
-    'Value': [50, 30, 20]
-})
-
-option = st.selectbox(
-    'Which option do you like best?',
-    df['Option'])
-
-st.write('You selected:', option)
-
-fig = px.pie(df, values='Value', names='Option')
-
-if option == 'Option 1':
-    st.write('You selected Option 1')
-    st.plotly_chart(fig)
-elif option == 'Option 2':
-    st.write('You selected Option 2')
-    st.plotly_chart(fig)
-else:
-    st.write('You selected Option 3')
-    st.plotly_chart(fig)
+if uploaded_file is not None:
+    df = pd.read_excel(uploaded_file)
+    st.write(df)
