03cd4c21c809ff14d08c7ee41574aa74866bca9f
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-16 14:55:49 +0900

    ee

--- MH/app.py
+++ MH/app.py
@@ -26,13 +26,13 @@ with tab1:
     ---
     '''
     option = st.selectbox(
-    '원하는 차트를 골라주세요',
-    ('Chart1', 'Chart2', 'Chart3'))
+    '원하는 차트 유형을 골라주세요',
+    ('Bar', 'Pie', 'Chart3'))
 
 st.write('고르신 차트를 출력하겠습니다:', option)
-if option == 'Chart1':
+if option == 'Bar':
     st.write("차트1입니다")
-elif option == 'Chart2':
+elif option == 'Pie':
     st.write("차트2입니다")
 elif option == 'Chart3':
     st.write("차트3입니다")    
