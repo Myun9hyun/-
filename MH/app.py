a09ba594e4b9cc5dbc4a2a3824caf13f721bf373
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-16 13:36:48 +0900

    ㅇ

--- MH/app.py
+++ MH/app.py
@@ -1,9 +1,2 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-# 엑셀 파일 불러오기
-uploaded_file = st.file_uploader("Choose an Excel file", type="xlsx")
-
-if uploaded_file is not None:
-    df = pd.read_excel(uploaded_file)
-    st.write(df)
+# Object notation
+st.sidebar.[element_name]
