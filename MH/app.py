7c18fc38a3c908b27718a92af19b4e6effa89d6d
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-16 16:58:59 +0900

    dd

--- MH/app.py
+++ MH/app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import matplotlib.pyplot as plt
-echo "backend: TkAgg" >> ~/.matplotlib/matplotlibrc
-import numpy as np
 
+import numpy as np
+echo "backend: TkAgg" >> ~/.matplotlib/matplotlibrc
 arr = np.random.normal(1, 1, size=100)
 fig, ax = plt.subplots()
 ax.hist(arr, bins=20)
