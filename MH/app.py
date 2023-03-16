9081b7cda52ae043708c84bee193f53b438a0b02
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-16 12:09:35 +0900

    ㅇ

--- MH/app.py
+++ MH/app.py
@@ -1,21 +1,7 @@
-# streamlit 라이브러리 호출
 import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt 
 
-import streamlit as st
-import pandas as pd
+option = st.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone'))
 
-# 엑셀 파일 불러오기
-uploaded_file = st.file_uploader("Choose an Excel file", type="xlsx")
-
-if uploaded_file is not None:
-    df = pd.read_excel(uploaded_file)
-    st.write(df)
-
-
-# https://pixabay.com/ko
-st.image(
-    "https://cdn.pixabay.com/photo/2020/01/26/21/57/computer-4796017_960_720.jpg"
-)
+st.write('You selected:', option)
