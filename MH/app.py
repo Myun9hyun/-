6d831863af9c0a9a22425fe04eb63053b7edc18f
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-16 11:58:24 +0900

    1

--- MH/app.py
+++ MH/app.py
@@ -1,22 +1,18 @@
 # streamlit 라이브러리 호출
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt 
+from google.colab import files
 
+df = pd.read_excel('KDX2021_SSC_ONLINE_DATA.xlsx')
+df2 = df.copy()
+df2
 #st.write 내부만 고치기!
 st.write(
     """
-   import streamlit as st
-import matplotlib.pyplot as plt
-
-# pie chart 데이터 생성
-labels = ['A', 'B', 'C', 'D']
-sizes = [15, 30, 45, 10]
-
-# pie chart 생성
-fig, ax = plt.subplots()
-ax.pie(sizes, labels=labels, autopct='%1.1f%%')
-ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-plt.title('Pie Chart Example')
-
+   
+   
 # streamlit에 pie chart 업로드
 st.pyplot(fig)
 
