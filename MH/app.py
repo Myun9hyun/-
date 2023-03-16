b21e985e5127ecd5f931fe5a439f837a241679b4
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-16 12:22:48 +0900

    ㅇ

--- MH/app.py
+++ MH/app.py
@@ -6,5 +6,24 @@ data = {'name': ['Alice', 'Bob', 'Charlie', 'David'],
         'age': [25, 30, 35, 40]}
 df = pd.DataFrame(data)
 
+
+
+option = st.selectbox(
+    'Which option do you like best?',
+    df['Option'])
+
+st.write('You selected:', option)
+
+fig = px.pie(df, values='Value', names='Option')
+
+if option == 'Option 1':
+    st.write('You selected Option 1')
+    st.plotly_chart(fig)
+elif option == 'Option 2':
+    st.write('You selected Option 2')
+    st.plotly_chart(fig)
+else:
+    st.write('You selected Option 3')
+    st.plotly_chart(fig)
 # 웹페이지에 데이터프레임 출력
 st.write(df)
