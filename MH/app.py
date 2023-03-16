cc8c71b35aed9c3aa692c94e72f8358ade3a8f33
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-16 15:09:50 +0900

    dd

--- MH/app.py
+++ MH/app.py
@@ -61,8 +61,8 @@ with tab3:
     tab3.write("추가적인 자료는 Google Colab 링크를 첨부해드립니다.")
     st.write()
     '''
-    [데이터 링크 1](https://www.google.com/)
-    [데이터 링크 2](https://www.google.com/)
+    > [데이터 링크 1](https://www.google.com/)
+    > [데이터 링크 2](https://www.google.com/)
     '''
 
 
