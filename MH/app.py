0386896b152baa4cd389e2bc77d6384782aa20d9
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-16 12:14:17 +0900

    ㅇ

--- MH/app.py
+++ MH/app.py
@@ -8,8 +8,20 @@ option = st.selectbox(
     ('연령대별 구매 카테고리', 'Home phone', 'Mobile phone'))
 
 st.write('You selected:', option)
-'''
-# 안
-'''
+{
+    labels = ['호박파이', '사과파이', '정어리파이', '엄마손파이']
+sizes = [15, 30, 45, 10]
+colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral']
+explode = (0, 0.1, 0, 0)
+plt.title("파이 차트")
+plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+        autopct='%1.1f%%', shadow=True, startangle=90) 
+# autopct-> auto percent 자동 확률 계산.
+# shadow -> 음영
+# startangle -> 0의 값이 시작되는 부분(제목 나오는 곳곳)
+plt.axis('equal')
+plt.show()
+}
+
 
 
