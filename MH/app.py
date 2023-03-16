bbc924c3432e284ded27b1d443394ebd920be0e0
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-16 13:34:42 +0900

    ㅇㅇ

--- MH/app.py
+++ MH/app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import pandas as pd
-
+import numpy as np
 # 엑셀 파일 불러오기
 uploaded_file = st.file_uploader("Choose an Excel file", type="xlsx")
 
 if uploaded_file is not None:
     df = pd.read_excel(uploaded_file)
     st.write(df)
+st.dataframe(df.style.highlight_max(axis=0))
