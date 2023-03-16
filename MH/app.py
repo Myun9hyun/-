f28594638d646412a4af52c2eb23c1073412bce2
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-16 12:25:41 +0900

    ㅇ

--- MH/app.py
+++ MH/app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import pandas as pd
+from google.colab import files
 
+df = pd.read_excel('KDX2021_SSC_ONLINE_DATA.xlsx')
+df2 = df.copy()
+df2
 # 데이터프레임 생성
-data = {'name': ['Alice', 'Bob', 'Charlie', 'David'],
-        'age': [25, 30, 35, 40]}
-df = pd.DataFrame(data)
+
 
 # 웹페이지에 데이터프레임 출력
 st.dataframe(df, width=500, height=500)
