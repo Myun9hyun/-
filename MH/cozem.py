8e0e4194829e2c29a0ae82fadc5ba1e7c85391d3
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-17 22:05:24 +0900

    ㅇㅇ

--- MH/cozem.py
+++ MH/cozem.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 
 tab1, tab2, tab3 = st.tabs(["🏠Homepage", "Cozem", "Novel"])
-
+st.sidebar.[element_name]
 with tab1:
    st.header("HomePage")
    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
@@ -12,8 +12,7 @@ with tab1:
 with tab2:
     st.header("코젬")
     st.image("https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2FMjAyMDEwMTBfMTkg%2FMDAxNjAyMzE0NjY1MTM3.OCHXBz1V9YHlZgKQWBqvgPyy8dKbnDj_sAMmoL67wWIg.2XpBx6CyawstsbtIl2UTMRJeE0VHPULU1OfbbzPVJkYg.JPEG%2FexternalFile.jpg&type=a340", width=400)
-    with st.sidebar:
-        st.[element_name]
+    
 with tab3:
    st.header("An owl")
    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
