2ba725a5829f7c847905b88ab24d509848211350
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-17 23:10:46 +0900

    ㅇㅇ

--- MH/cozem.py
+++ MH/cozem.py
@@ -54,17 +54,17 @@ elif choice == "Event_reward":
             answer = suro(s) + flag_cozem(f_input)
             return int(answer)
 
-        if st.button("계산하기"):
-            result_suro = suro(s_input)
-            cozem_sums.append(cozem_sum(s_input))
-            st.write(f"플래그 점수 {f_input}점, 수로 점수 {s_input}에 따른 코젬은 {int(cozem_sums[-1])}개 입니다.")
+       
         while True:
             name = st.text_input("이름을 입력하세요 (종료는 엔터): ")
             if name:
                 weekly_mission = st.number_input("주간 입력 : ")
-                
-        cozem_sums.append(result)
-        st.write(f"{name}님의 코젬은 {int(result)}개 입니다.")
+            if st.button("계산하기"):
+                result_suro = suro(s_input)
+                cozem_sums.append(cozem_sum(s_input))
+        st.write(f"플래그 점수 {f_input}점, 수로 점수 {s_input}에 따른 코젬은 {int(cozem_sums[-1])}개 입니다.")
+            cozem_sums.append(result)
+            st.write(f"{name}님의 코젬은 {int(result)}개 입니다.")
     with tab3:
         st.header("An owl")
         st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
