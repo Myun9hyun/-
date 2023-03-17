a6e2854c56ef35bb2767db82b417439b12801b86
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-17 22:12:46 +0900

    d

--- MH/cozem.py
+++ MH/cozem.py
@@ -16,21 +16,21 @@ choice = st.sidebar.selectbox("Select an option", menu)
 # 선택된 메뉴에 따라 다른 탭 출력
 if choice == "Home":
     st.write("Welcome to the Home page")
+    
+
+    tab1, tab2, tab3 = st.tabs(["🏠Homepage", "Cozem", "Novel"])
+    with tab1:
+    st.header("HomePage")
+    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+    with tab2:
+        st.header("코젬")
+        st.image("https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2FMjAyMDEwMTBfMTkg%2FMDAxNjAyMzE0NjY1MTM3.OCHXBz1V9YHlZgKQWBqvgPyy8dKbnDj_sAMmoL67wWIg.2XpBx6CyawstsbtIl2UTMRJeE0VHPULU1OfbbzPVJkYg.JPEG%2FexternalFile.jpg&type=a340", width=400)
+        
+    with tab3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
 elif choice == "About":
     st.write("Welcome to the About page")
 else:
     st.write("Welcome to the Contact page")
-
-
-tab1, tab2, tab3 = st.tabs(["🏠Homepage", "Cozem", "Novel"])
-with tab1:
-   st.header("HomePage")
-   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-
-with tab2:
-    st.header("코젬")
-    st.image("https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2FMjAyMDEwMTBfMTkg%2FMDAxNjAyMzE0NjY1MTM3.OCHXBz1V9YHlZgKQWBqvgPyy8dKbnDj_sAMmoL67wWIg.2XpBx6CyawstsbtIl2UTMRJeE0VHPULU1OfbbzPVJkYg.JPEG%2FexternalFile.jpg&type=a340", width=400)
-    
-with tab3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
