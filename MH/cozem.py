f6651e6a5dc5fb5b656a7f70585a27f922d2111f
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-17 21:58:44 +0900

    ㅇ

--- MH/cozem.py
+++ MH/cozem.py
@@ -3,15 +3,15 @@ import numpy as np
 import streamlit as st
 
 
-tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+tab1, tab2, tab3 = st.tabs(["🏠Homepage", "Cozem", "Novel"])
 
 with tab1:
-   st.header("A cat")
+   st.header("HomePage")
    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
 
 with tab2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+   st.header("코젬")
+   st.image("https://upload3.inven.co.kr/upload/2021/02/07/bbs/i13327956984.jpg?MW=800", width=200)
 
 with tab3:
    st.header("An owl")
