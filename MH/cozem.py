fefd0b930b6a78ec7ff80d2f2128eed46d3e6725
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-17 23:29:26 +0900

    ㅇㅇ

--- MH/cozem.py
+++ MH/cozem.py
@@ -67,7 +67,7 @@ elif choice == "Event_reward":
             # st.write(f"플래그 점수 {f_input}점, 수로 점수 {s_input}에 따른 코젬은 {int(cozem_sums[-1])}개 입니다.")
             # st.write(f"플래그 점수 {f}점, 수로 점수 {s}에 따른 코젬은 {cozem_sums}개 입니다.")
             
-            st.write(f"플래그 점수 {f}점, 수로 점수 {s}에 따른 코젬은 {int(cozem_sums[-1])}개 입니다.")
+                st.write(f"플래그 점수 {f}점, 수로 점수 {s}에 따른 코젬은 {int(cozem_sums[-1])}개 입니다.")
         
 
         # cozem_sums.append(result)
