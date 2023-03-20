1bbe17832f682598f4d1c3f0392d5cce0041ea51
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-20 14:46:51 +0900

    ㅇㅇ

--- MH/code.py
+++ MH/code.py
@@ -13,18 +13,13 @@ st.write(df)
 
 # 입력 폼 생성
 st.header('새로운 데이터 추가')
-while True:
-    name = st.text_input('이름')
-    age = st.number_input('나이')
-    gender = st.selectbox('성별', ['여성', '남성'])
-    
-    # "Add Row" 버튼 클릭시 새로운 행 추가
-    if st.button('Add Row'):
-        new_row = {'이름': name, '나이': age, '성별': gender}
-        df = df.append(new_row, ignore_index=True)
-        st.write('새로운 데이터가 추가되었습니다.')
-        st.dataframe(df)
-    
-    # "Add More" 버튼 클릭시 새로운 데이터 추가 입력
-    if not st.button('Add More'):
-        break
+name = st.text_input('이름')
+age = st.number_input('나이', min_value=0)
+gender = st.selectbox('성별', ['여성', '남성'])
+
+# "Add Row" 버튼 클릭시 새로운 행 추가
+if st.button('Add Row'):
+    new_row = {'이름': name, '나이': age, '성별': gender}
+    df = df.append(new_row, ignore_index=True)
+    st.write('새로운 데이터가 추가되었습니다.')
+    st.write(df)
