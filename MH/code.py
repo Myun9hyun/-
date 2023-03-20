d2bef3c5a9226ca1b6ff2b22a24203f86145fc0c
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-20 17:04:14 +0900

    ㅇㅇ

--- MH/code.py
+++ MH/code.py
@@ -6,22 +6,21 @@ name_list = []
 score_list = []
 
 # 이름과 점수를 입력받는 함수
-def input_name_score(idx):
-    name = st.text_input("이름을 입력하세요:", key=f"name_{idx}")
-    score = st.number_input("점수를 입력하세요:", min_value=0, max_value=100, step=1, key=f"score_{idx}")
+def input_name_score():
+    name = st.text_input("이름을 입력하세요:")
+    score = st.number_input("점수를 입력하세요:", min_value=0, max_value=100, step=1)
     return name, score
 
 # Streamlit 앱 생성
 st.title("이름과 점수 입력하기")
 
 # 이름과 점수 입력 받기
-idx = 0
-while st.button("입력", key=f"input_{idx}"):
-    name, score = input_name_score(idx)
-    if name and score:
-        name_list.append(name)
-        score_list.append(score)
-    idx += 1
+while True:
+    name, score = input_name_score()
+    if not name or not score:
+        break
+    name_list.append(name)
+    score_list.append(score)
 
 # '표 만들기' 버튼을 누르면 데이터프레임으로 표시
 if st.button("표 만들기"):
