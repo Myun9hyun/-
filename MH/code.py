1076b5797ec41baf5f036237e80baca5c12a4fab
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-21 10:00:15 +0900

    ㅇㅇ

--- MH/code.py
+++ MH/code.py
@@ -1,8 +1,8 @@
 import streamlit as st
+from streamlit.state.session_state import SessionState
 
-# 이름과 점수를 저장할 리스트를 생성합니다.
-name_list = []
-score_list = []
+# SessionState 객체를 생성합니다.
+session_state = SessionState.get(name_list=[], score_list=[])
 
 # 입력 폼을 생성합니다.
 st.write('이름과 점수를 입력하세요.')
@@ -11,11 +11,11 @@ score = st.number_input('점수')
 
 # '추가' 버튼을 누르면 입력받은 값을 리스트에 추가합니다.
 if st.button('추가'):
-    name_list.append(name)
-    score_list.append(score)
+    session_state.name_list.append(name)
+    session_state.score_list.append(score)
     st.write('이름: {}, 점수: {}'.format(name, score))
 
 # '종료' 버튼을 누르면 리스트를 출력합니다.
 if st.button('종료'):
-    st.write('이름 리스트: {}'.format(name_list))
-    st.write('점수 리스트: {}'.format(score_list))
+    st.write('이름 리스트: {}'.format(session_state.name_list))
+    st.write('점수 리스트: {}'.format(session_state.score_list))
