f461a1be81ec7015206aeb61aa5f8a3b5bfcdceb
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-21 10:03:15 +0900

    dd

--- MH/code.py
+++ MH/code.py
@@ -17,6 +17,10 @@ if st.button('추가'):
     st.write('이름 리스트: {}'.format(name_list))
     st.write('점수 리스트: {}'.format(score_list))
 
+    if st.button('종료'):
+        st.write('이름 리스트: {}'.format(name_list))
+        st.write('점수 리스트: {}'.format(score_list))
+
 # '종료' 버튼을 누르면 리스트를 출력합니다.
 if st.button('종료'):
     st.write('이름 리스트: {}'.format(name_list))
