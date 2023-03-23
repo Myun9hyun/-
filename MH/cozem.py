b1d2ad56c52e8a1bf3a3476c975710bfa5f5e5ae
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-23 21:04:31 +0900

    dd

--- MH/cozem.py
+++ MH/cozem.py
@@ -33,7 +33,7 @@ if choice == "메인페이지":
     | 부마 | 둥둥향 | 캐논슈터 | [https://img.icons8.com/color/256/kakaotalk.png](https://open.kakao.com/o/sl6WBJUc) |
     | 부마 | 돌체라페  | 메르세데스 | [https://img.icons8.com/color/256/kakaotalk.png](https://open.kakao.com/o/sEmQw9Ye) |
 
-    [![GitHub](https://badgen.net/badge/icon/apple?icon=apple&label)](https://open.kakao.com/o/spPPOAhc)
+    [![GitHub](https://img.icons8.com/color/256/kakaotalk.png)](https://open.kakao.com/o/spPPOAhc)
     ---
     '''
 
