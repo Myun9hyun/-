d3ab4c404aca29e0243f56ded993a6d59e387dac
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-24 12:41:45 +0900

    dd

--- MH/practice.py
+++ MH/practice.py
@@ -151,6 +151,7 @@ elif choice == "데이터페이지":
                 st.dataframe(df)
                 win_rate = ((df['W'] / df['G']) * 100)
                 win_rate = win_rate.round(2)
+                win_rate.columns = ['TEAM': ['North Carolina', 'Wisconsin', 'Michigan', 'Texas Tech']]
                 win_rate_t = win_rate.T
                 st.dataframe(win_rate_t)
                 fig = px.bar(win_rate_t)
