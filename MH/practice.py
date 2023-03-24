d51fca03ad0ad83dad7203c2d752ba32ebef8858
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-24 15:07:37 +0900

    dd

--- MH/practice.py
+++ MH/practice.py
@@ -126,7 +126,7 @@ elif choice == "데이터페이지":
         url = "https://raw.githubusercontent.com/Myun9hyun/trash/main/MH/cbb.csv"
         df = pd.read_csv(url)
         st.write(df)
-        df_data = st.text_input('검색하고 싶은 데이터를 입력해 주세요 : ')
+        df_data = st.text_input('검색하고 싶은 index를 입력해 주세요 : ')
         # data = {'Team': ['A', 'B', 'C', 'A', 'B', 'C'], 'Score': [10, 20, 30, 40, 50, 60]}
         # team_name = st.text_input('Enter a team name')
         filtered_df = df[df.apply(lambda row: df_data.lower() in row.astype(str).str.lower().values.tolist(), axis=1)]
