32dfc0620d69e0a9ba717f9cc835fdbc7986d3b2
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-26 22:43:14 +0900

    ㅇㅇ

--- MH/demo.py
+++ MH/demo.py
@@ -1,18 +1,27 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 import plotly.graph_objects as go
 
+# CSV 파일을 데이터프레임으로 읽어오기
 df = pd.read_csv("https://raw.githubusercontent.com/Myun9hyun/trash/main/MH/Basketball_processing.csv")
 
-selected_stats = st.multiselect('Select stats', df.columns)
+# 지역(CONF)에 해당하는 행 추출
+conf = st.sidebar.selectbox('Select a conference', df['CONF'].unique())
+conf_df = df[df['CONF'] == conf]
+
+# 시즌(YEAR)에 해당하는 행 추출
+year = st.sidebar.selectbox('Select a season', conf_df['YEAR'].unique())
+year_df = conf_df[conf_df['YEAR'] == year]
+
+# 선택한 스탯들을 multiselect로 선택
+selected_stats = st.multiselect('Select stats', year_df.columns)
 
 if selected_stats:
     fig = go.Figure()
     for stat in selected_stats:
         fig.add_trace(go.Scatterpolar(
-            r=[df[stat].mean()],
-            theta=[stat],
+            r=year_df[stat],
+            theta=year_df.index,
             fill='toself',
             name=stat
         ))
@@ -20,7 +29,7 @@ if selected_stats:
         polar=dict(
             radialaxis=dict(
                 visible=True,
-                range=[df[selected_stats].min().min(), df[selected_stats].max().max()]
+                range=[year_df[selected_stats].min().min(), year_df[selected_stats].max().max()]
             )
         ),
         showlegend=True
