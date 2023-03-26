423629b298bc16b85ef9547811bd846ca748149e
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-26 21:41:49 +0900

    dd

--- MH/demo.py
+++ MH/demo.py
@@ -21,33 +21,26 @@ if uploaded_file is not None:
        'TOR', 'TORD', 'ORB', 'DRB', 'FTR', 'FTRD', '2P_O', '2P_D', '3P_O',
        '3P_D', 'ADJ_T', 'WAB', 'POSTSEASON', 'SEED', 'YEAR'])
 
-    # 추출된 데이터 내에서 선택된 스탯만 추출
-    stats_df = filtered_df[['TEAM'] + selected_stats]
+    # 선택된 스탯 리스트
+    # selected_stats = st.multiselect('Select stats to compare', list(df.columns[4:]))
 
-    # Radar chart 그리기
+    # 선택된 스탯만 포함하는 데이터프레임
+    df_selected = df.loc[df['CONF'] == location].reset_index(drop=True)[['TEAM', 'YEAR'] + selected_stats]
+
+    # 스탯 비교 레이더 차트 그리기
+    fig, ax = plt.subplots(figsize=(10, 8))
     categories = selected_stats
     N = len(categories)
-    values = stats_df.iloc[:, 1:].values.tolist()
-    values += values[:1]
-    angles = [n / float(N) * 2 * pi for n in range(N)]
+    angles = [n / float(N) * 2 * math.pi for n in range(N)]
     angles += angles[:1]
-
-    fig, ax = plt.subplots(figsize=(6, 6), subplot_kw=dict(polar=True))
-
-    ax.set_theta_offset(pi / 2)
+    ax.set_theta_offset(math.pi / 2)
     ax.set_theta_direction(-1)
     plt.xticks(angles[:-1], categories)
-
     ax.set_rlabel_position(0)
-    plt.yticks([0.25, 0.5, 0.75], ["0.25", "0.50", "0.75"], color="grey", size=7)
-    plt.ylim(0, 1)
-
-    for i in range(len(stats_df)):
-        values = stats_df.iloc[i, 1:].values.flatten().tolist()
+    for i in range(len(df_selected)):
+        values = df_selected.loc[i, selected_stats].values.flatten().tolist()
         values += values[:1]
-        ax.plot(angles, values, linewidth=1, linestyle='solid', label=stats_df.iloc[i, 0])
-        ax.fill(angles, values, 'b', alpha=0.1)
-
+        ax.plot(angles, values, linewidth=1, linestyle='solid', label=df_selected.loc[i, 'TEAM'])
+        ax.fill(angles, values, alpha=0.1)
     plt.legend(loc='upper right', bbox_to_anchor=(0.1, 0.1))
-
     st.pyplot(fig)
