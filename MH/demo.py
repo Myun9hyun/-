d7404b725cce70ee3ffcbd9e6663f1d61aa1b371
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-26 23:00:13 +0900

    ㅇㅇ

--- MH/demo.py
+++ MH/demo.py
@@ -4,10 +4,14 @@ import streamlit as st
 # 데이터프레임을 불러옵니다.
 df = pd.read_csv("https://raw.githubusercontent.com/Myun9hyun/trash/main/MH/Basketball_processing.csv")
 
-# 멀티셀렉트로 선택한 열을 입력받습니다.
-selected_cols = st.multiselect('Select columns', df.columns)
+# 체크박스로 선택한 columns 받기
+selected_columns = st.multiselect('Select columns', df.columns)
 
-# 선택한 열에 해당하는 인덱스를 추출합니다.
-df_selected = df.loc[:, selected_cols]
-selected_indices = df_selected.index.tolist()
-st.write(selected_indices)
+# 선택한 columns에 해당하는 index 추출하기
+selected_index = df[df[selected_columns].notna().all(axis=1)].index
+
+# 선택한 index들을 출력할 데이터프레임 만들기
+selected_df = df.loc[selected_index, selected_columns]
+
+# 선택한 index들을 출력
+st.write(selected_df)
