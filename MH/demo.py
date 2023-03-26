63c25a18d87634af43e7e129cd59801789b60811
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-26 22:51:57 +0900

    ㅇㅇ

--- MH/demo.py
+++ MH/demo.py
@@ -1,38 +1,12 @@
-import streamlit as st
 import pandas as pd
-import plotly.graph_objects as go
+import streamlit as st
 
-# Load data
+# 데이터프레임을 불러옵니다.
 df = pd.read_csv("https://raw.githubusercontent.com/Myun9hyun/trash/main/MH/Basketball_processing.csv")
 
-# User input for CONF and YEAR
-conf = st.text_input('Enter a CONF value')
-year = st.text_input('Enter a YEAR value')
+# 멀티셀렉트로 선택한 열을 입력받습니다.
+selected_cols = st.multiselect('Select columns', df.columns)
 
-# Extract rows matching CONF and YEAR
-filtered_df = df[df['CONF'] == conf]
-filtered_df = filtered_df[filtered_df['YEAR'] == year]
-
-# Multiselect for stats selection
-selected_stats = st.multiselect('Select stats', filtered_df.columns)
-
-# Radar chart
-if selected_stats:
-    fig = go.Figure()
-    for stat in selected_stats:
-        fig.add_trace(go.Scatterpolar(
-            r=filtered_df[stat],
-            theta=filtered_df.index,
-            fill='toself',
-            name=stat
-        ))
-    fig.update_layout(
-        polar=dict(
-            radialaxis=dict(
-                visible=True,
-                range=[filtered_df[selected_stats].min().min(), filtered_df[selected_stats].max().max()]
-            )
-        ),
-        showlegend=True
-    )
-    st.plotly_chart(fig, use_container_width=True)
+# 선택한 열에 해당하는 인덱스를 추출합니다.
+df_selected = df.loc[:, selected_cols]
+selected_indices = df_selected.index.tolist()
