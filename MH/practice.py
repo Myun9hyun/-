8272b2a62d09803b336ef3807a724a2e774b898e
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-26 12:47:08 +0900

    dd

--- MH/practice.py
+++ MH/practice.py
@@ -311,23 +311,26 @@ elif choice == "데이터페이지":
                 st.write("막대 차트 2입니다")
                 url = "https://raw.githubusercontent.com/Myun9hyun/trash/main/MH/cbb.csv"
                 df = pd.read_csv(url)
+                options = st.selectbox(
+                '검색하고 싶은 columns를 골라주세요',
+                ('CONF', 'TEAM'))
+                if option == 'CONF':
+                    # 'year' 열에서 고유값 추출
+                    unique_conf = df['CONF'].unique().astype(str)
 
-                # 'year' 열에서 고유값 추출
-                unique_year = df['YEAR'].unique().astype(str)
+                    # 각 고유값에 해당하는 인덱스 추출하여 딕셔너리에 저장
+                    index_dict = {}
+                    for CONF in unique_conf:
+                        index_dict[CONF] = df[df['CONF'] == CONF].index.tolist()
 
-                # 각 고유값에 해당하는 인덱스 추출하여 딕셔너리에 저장
-                index_dict = {}
-                for YEAR in unique_year:
-                    index_dict[YEAR] = df[df['YEAR'] == int(YEAR)].index.tolist()
-
-                # 사용자가 입력한 고유값에 해당하는 행 출력
-                user_input = st.text_input(f"Enter a year from {', '.join(unique_year)}:")
-                if user_input in unique_year:
-                    indices = index_dict[user_input]
-                    sub_df = df.loc[indices]
-                    st.write(sub_df)
-                else:
-                    st.write("Invalid input.")
+                    # 사용자가 입력한 고유값에 해당하는 행 출력
+                    user_input = st.text_input(f"Enter a year from {', '.join(unique_conf)}:")
+                    if user_input in unique_conf:
+                        indices = index_dict[user_input]
+                        sub_df = df.loc[indices]
+                        st.write(sub_df)
+                    else:
+                        st.write("Invalid input.")
 
             elif option == 'Bar3':
                 st.write("막대 차트 3입니다")
