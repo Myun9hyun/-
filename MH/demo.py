9f1bd03f0e25d9b71b8b9ccc9b546d3c3cef633b
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-26 15:06:04 +0900

    ㅇㅇ

--- MH/demo.py
+++ MH/demo.py
@@ -5,19 +5,26 @@ url = "https://raw.githubusercontent.com/Myun9hyun/trash/main/MH/Basketball_proc
 
 df = pd.read_csv("https://raw.githubusercontent.com/Myun9hyun/trash/main/MH/Basketball_processing.csv", index_col=0)
 
-conf_list = list(df["CONF"].unique())
-conf_choice = st.sidebar.selectbox("Select a conference", conf_list)
+# 선택한 CONF 값에 해당하는 데이터프레임 출력
+conf = st.sidebar.selectbox("Select Conference", df["CONF"].unique())
+selected_df = df[df["CONF"] == conf]
 
-filtered_df = df[df["CONF"] == conf_choice]
+# 선택한 YEAR 값에 해당하는 데이터프레임 출력
+year = st.sidebar.selectbox("Select Year", selected_df["YEAR"].unique())
+selected_df = selected_df[selected_df["YEAR"] == year]
 
-year_list = list(filtered_df["YEAR"].unique())
-year_choice = st.sidebar.selectbox("Select a year", year_list)
+# 선택한 columns에 해당하는 데이터프레임 출력
+columns = st.multiselect("Select Columns", selected_df.columns)
+selected_df = selected_df[columns]
 
-selected_cols = st.multiselect("Select columns to display", list(filtered_df.columns))
+# 각 팀들의 Radar Chart를 그리기 위해 팀명을 index로 지정
+selected_df.set_index(selected_df.index, inplace=True)
 
-display_df = filtered_df[filtered_df["YEAR"] == year_choice][selected_cols]
+# Radar Chart 그리기
+fig = px.line_polar(selected_df, r=selected_df.values, theta=selected_df.columns, line_close=True)
 
-if not display_df.empty:
-    st.plotly_chart(px.line_polar(display_df, theta=selected_cols, r=display_df.iloc[0].values))
-else:
-    st.warning("No data found for selected filters.")
+# Radar Chart에 팀명 추가
+for d in fig.data:
+    d.name = d.name.split("_")[0]
+
+st.plotly_chart(fig)
