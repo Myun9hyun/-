bed5451e95b3812953d8d8dc988757605a0b2074
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-26 15:11:04 +0900

    ㅇㅇ

--- MH/demo.py
+++ MH/demo.py
@@ -1,30 +1,44 @@
-import pandas as pd
 import streamlit as st
-import plotly.express as px
-import numpy as np
+import pandas as pd
+from math import pi
+from bokeh.palettes import Category10
+from bokeh.plotting import figure
+
 url = "https://raw.githubusercontent.com/Myun9hyun/trash/main/MH/Basketball_processing.csv"
 
 df = pd.read_csv("https://raw.githubusercontent.com/Myun9hyun/trash/main/MH/Basketball_processing.csv", index_col=0)
 
-conf = st.sidebar.selectbox("Select Conference", df["CONF"].unique())
-selected_df = df[df["CONF"] == conf]
+import streamlit as st
+import pandas as pd
+from math import pi
+from bokeh.palettes import Category10
+from bokeh.plotting import figure
 
-# 선택한 YEAR 값에 해당하는 데이터프레임 출력
-year = st.sidebar.selectbox("Select Year", selected_df["YEAR"].unique())
-selected_df = selected_df[selected_df["YEAR"] == year]
+# csv 파일 읽어오기
+df = pd.read_csv("your_csv_file.csv")
 
-# 선택한 columns에 해당하는 데이터프레임 출력
-columns = st.multiselect("Select Columns", selected_df.columns)
-selected_df = selected_df[columns]
+# 선택할 수 있는 옵션 리스트
+conf_list = df['CONF'].unique().tolist()
+conf_selected = st.sidebar.selectbox("Select Conference", conf_list)
 
-# 각 팀들의 Radar Chart를 그리기 위해 팀명을 index로 지정
-selected_df.set_index(selected_df.index, inplace=True)
+# 선택한 컨퍼런스에 해당하는 행들 추출
+filtered_df = df[df['CONF'] == conf_selected]
 
-# Radar Chart 그리기
-fig = px.line_polar(selected_df, r=selected_df.values, theta=selected_df.columns, line_close=True)
+# 선택할 수 있는 옵션 리스트
+columns = filtered_df.columns[3:].tolist()
+columns_selected = st.sidebar.multiselect("Select Columns", columns)
 
-# Radar Chart에 팀명 추가
-for d in fig.data:
-    d.name = d.name.split("_")[0]
+# 선택한 컬럼들로 radar chart 그리기
+colors = Category10[10]
+p = figure(title="Radar Chart", plot_width=500, plot_height=500)
 
-st.plotly_chart(fig)
+for i, column in enumerate(columns_selected):
+    data = filtered_df[column].tolist()
+    angles = [n / float(len(columns_selected)) * 2 * pi for n in range(len(columns_selected))]
+    angles += angles[:1]
+    data += data[:1]
+    p.annular_wedge(x=0, y=0, inner_radius=0.2, outer_radius=0.8, start_angle=angles[i], end_angle=angles[i+1], color=colors[i], alpha=0.5)
+    p.line(x=data, y=data, color=colors[i], alpha=0.5)
+    p.patch(x=data, y=data, color=colors[i], alpha=0.2)
+
+st.bokeh_chart(p, use_container_width=True)
