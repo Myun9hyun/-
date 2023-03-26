1ee8baba3ae7b7724d6065fb410fb36b84fc45a2
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-26 23:12:07 +0900

    ㅇㅇ

--- MH/demo.py
+++ MH/demo.py
@@ -18,7 +18,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
-
+import numpy as np
 # 데이터 불러오기
 df = pd.read_csv("https://raw.githubusercontent.com/Myun9hyun/trash/main/MH/Basketball_processing.csv")
 
@@ -30,6 +30,30 @@ selected_index = df[df[selected_columns].notna().all(axis=1)].index
 
 # 선택한 index들을 출력할 데이터프레임 만들기
 selected_df = df.loc[selected_index, selected_columns]
+
+
+# 해당 컬럼에 결측값이 있을 경우 0으로 대체
+selected_df[col] = selected_df[col].fillna(0)
+
+# 선택한 스탯들을 비교하는 radar chart 그리기
+fig = go.Figure()
+for col in selected_stats:
+    fig.add_trace(go.Scatterpolar(
+        r=[selected_df[col].mean()],
+        theta=[col],
+        fill='toself',
+        name=col
+    ))
+fig.update_layout(
+    polar=dict(
+        radialaxis=dict(
+            visible=True,
+            range=[selected_df[selected_stats].min().min(), selected_df[selected_stats].max().max()]
+        )
+    ),
+    showlegend=True
+)
+st.plotly_chart(fig, use_container_width=True)
 
 # radar chart 그리기
 if not selected_df.empty:
