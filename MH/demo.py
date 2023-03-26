bb1036da12251696c2c8340e2eabcae49b563889
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-26 21:24:55 +0900

    ㅇㅇ

--- MH/demo.py
+++ MH/demo.py
@@ -1,41 +1,52 @@
-# Streamlit 패키지 불러오기
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 from math import pi
 
-# CSV 파일 업로드  
+# csv 파일 업로드
 uploaded_file = st.file_uploader("Choose a file")
-
-# CSV 파일이 업로드된 경우
 if uploaded_file is not None:
-    # 업로드된 파일을 데이터프레임으로 변환
     df = pd.read_csv(uploaded_file)
-    # CONF 열에서 유니크한 값 추출
-    confs = df['CONF'].unique().tolist()
-    # 사용자가 선택한 CONF 값
-    selected_conf = st.selectbox('Select a conference', confs)
-    # 선택된 CONF에 해당하는 행 추출
-    selected_rows = df[df['CONF'] == selected_conf]
-    # YEAR 열에서 유니크한 값 추출
-    years = selected_rows['YEAR'].unique().tolist()
-    # 사용자가 선택한 YEAR 값
-    selected_year = st.selectbox('Select a year', years)
-    # 선택된 YEAR에 해당하는 행 추출
-    selected_row = selected_rows[selected_rows['YEAR'] == selected_year]
-    # 사용자가 선택한 columns
-    columns = st.multiselect('Select columns', list(selected_row.columns))
-    # 선택된 columns에 해당하는 값 추출
-    values = selected_row.loc[:, columns].values.flatten().tolist()
-    # radar chart 그리기
-    angles = [n / float(len(columns)) * 2 * pi for n in range(len(columns))]
+
+    # 지역 입력
+    location = st.text_input('Enter the location')
+    filtered_df = df[df['CONF'] == location]
+
+    # 시즌 입력
+    season = st.text_input('Enter the season')
+    filtered_df = filtered_df[filtered_df['YEAR'] == season]
+
+    # 스탯 선택
+    selected_stats = st.multiselect('Select stats', ['TEAM', 'CONF', 'G', 'W', 'ADJOE', 'ADJDE', 'BARTHAG', 'EFG_O', 'EFG_D',
+       'TOR', 'TORD', 'ORB', 'DRB', 'FTR', 'FTRD', '2P_O', '2P_D', '3P_O',
+       '3P_D', 'ADJ_T', 'WAB', 'POSTSEASON', 'SEED', 'YEAR'])
+
+    # 추출된 데이터 내에서 선택된 스탯만 추출
+    stats_df = filtered_df[['TEAM'] + selected_stats]
+
+    # Radar chart 그리기
+    categories = selected_stats
+    N = len(categories)
+    values = stats_df.iloc[:, 1:].values.tolist()
+    values += values[:1]
+    angles = [n / float(N) * 2 * pi for n in range(N)]
     angles += angles[:1]
+
     ax = plt.subplot(111, polar=True)
     ax.set_theta_offset(pi / 2)
     ax.set_theta_direction(-1)
-    plt.xticks(angles[:-1], columns)
+    plt.xticks(angles[:-1], categories)
+
     ax.set_rlabel_position(0)
-    plt.yticks([], [])
-    ax.plot(angles, values, linewidth=1, linestyle='solid')
-    ax.fill(angles, values, 'b', alpha=0.1)
+    plt.yticks([0.25, 0.5, 0.75], ["0.25", "0.50", "0.75"], color="grey", size=7)
+    plt.ylim(0, 1)
+
+    for i in range(len(stats_df)):
+        values = stats_df.iloc[i, 1:].values.flatten().tolist()
+        values += values[:1]
+        ax.plot(angles, values, linewidth=1, linestyle='solid', label=stats_df.iloc[i, 0])
+        ax.fill(angles, values, 'b', alpha=0.1)
+
+    plt.legend(loc='upper right', bbox_to_anchor=(0.1, 0.1))
+
     st.pyplot()
