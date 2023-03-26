a683b1a222d4d9f7882c3142301e4dda404ce003
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-26 12:51:50 +0900

    ㅇㅇ

--- MH/practice.py
+++ MH/practice.py
@@ -316,6 +316,8 @@ elif choice == "데이터페이지":
                 ('CONF', 'TEAM'))
                 if option == 'CONF':
                     # 'year' 열에서 고유값 추출
+                     url = "https://raw.githubusercontent.com/Myun9hyun/trash/main/MH/cbb.csv"
+                    df = pd.read_csv(url)
                     unique_conf = df['CONF'].unique()
 
                     # 각 고유값에 해당하는 인덱스 추출하여 딕셔너리에 저장
