2ff3491812188f5db24b21b38ad8a8ad701e8b74
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-27 14:35:16 +0900

    d

--- MH/demo.py
+++ MH/demo.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pickle
 import numpy as np
 # 모델 불러오기
-model_path = "MH/model.pkl"
+model_path = "MH/model_RF.pkl"
 with open(model_path, 'rb') as f:
     model = pickle.load(f)
 
