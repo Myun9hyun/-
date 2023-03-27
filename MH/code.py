60c8ced53b70f0f591d6738477a357eaa81690b6
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-27 15:26:23 +0900

    ㅇㅇ

--- MH/code.py
+++ MH/code.py
@@ -3,7 +3,7 @@ import numpy as np
 import streamlit as st
 
 # 선형회귀 모델 불러오기
-model_path = "MH/model.pkl"
+model_path = "MH/model_LR.pkl"
 with open(model_path, 'rb') as f:
     model = pickle.load(f)
 
@@ -14,12 +14,11 @@ st.write('입력 변수')
 # 입력 변수를 위한 슬라이더 추가
 x = st.slider('X', 0.0, 1.0, 0.5, 0.01)
 x = np.array([x]*77).reshape(1, -1)  # 입력값의 차원을 맞춰줍니다.
+
+# 모델을 사용하여 예측 수행
 y = model.predict(x)
 y = y * 100
 y = y.round(2)
-# 모델을 사용하여 예측 수행
-x = np.array(x).reshape(1, -1)
-y = model.predict(x)
 
 # 예측 결과 출력
 st.subheader('예측 결과')
