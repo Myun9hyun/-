f67e19ab9827014b856878d4496c2a962ffa4874
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-27 12:32:29 +0900

    ㅇㅇ

--- MH/demo.py
+++ MH/demo.py
@@ -10,7 +10,8 @@ with open(model_path, 'rb') as f:
 
 # 사용자가 입력한 경기수와 승리경기수를 기반으로 승률을 예측합니다.
 def predict_win_rate(wins, games):
-    x = np.array([wins, games]*77).reshape(77, 2)  # 입력값의 차원을 맞춰줍니다.
+    x = np.tile(np.array([wins, games]), (77,1))
+      # 입력값의 차원을 맞춰줍니다.
     win_rate = model.predict(x)
     return win_rate[0]
 
@@ -26,7 +27,8 @@ def main():
 
     # 예측 결과를 표시합니다.
     if st.button("예측하기"):
-        x = np.array([wins, games]*77).reshape(77,2)  # 입력값의 차원을 맞춰줍니다.
+        x = np.tile(np.array([wins, games]), (77,1))
+  # 입력값의 차원을 맞춰줍니다.
         win_rate = model.predict(x)
         st.write(f"예상 승률: {win_rate.mean():.2%}")
 
