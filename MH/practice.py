32b435892dc65dbe158c7567f7974ea46f8438e5
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-27 11:33:55 +0900

    ㅇㅇ

--- MH/practice.py
+++ MH/practice.py
@@ -331,28 +331,27 @@ elif choice == "데이터페이지":
         option = st.selectbox(
         '원하는 차트를 골라주세요',
         ('Chart1', 'Chart2', 'Chart3'))
-            if option == 'Chart1':
-                # 모델 불러오기
-                model_path = "MH/model.pkl"
+        if option == 'Chart1':
+            # 모델 불러오기
+            model_path = "MH/model.pkl"
+            with open(model_path, 'rb') as f:
+            model = pickle.load(f)
 
-                with open(model_path, 'rb') as f:
-                    model = pickle.load(f)
-
-                st.title('Linear Regression Model')
+            st.title('Linear Regression Model')
 
                 # create sidebar with input parameters
                 # st.sidebar.header('Input Parameters')
-                st.write('Input Parameters')
+            st.write('Input Parameters')
                 # x = st.sidebar.slider('X', 0.0, 10.0, 5.0, 0.1)
-                x = st.slider('X', 0.0, 1.0, 0.5, 0.01)
+            x = st.slider('X', 0.0, 1.0, 0.5, 0.01)
 
                 # use model to make prediction
-                x = np.array([x]*77).reshape(1, -1)  # 입력값의 차원을 맞춰줍니다.
-                y = model.predict(x)
+            x = np.array([x]*77).reshape(1, -1)  # 입력값의 차원을 맞춰줍니다.
+            y = model.predict(x)
 
                 # show prediction result
-                st.subheader('Prediction Result')
-                st.write('Y:', y[0])
+            st.subheader('Prediction Result')
+            st.write('Y:', y[0])
     with tab3:
         tab3.subheader("Streamlit 진행상태..")
         st.write()
