da6388f7be9586c515e3569801bbad00fd46762f
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-27 12:34:11 +0900

    ㅇㅇ

--- MH/demo.py
+++ MH/demo.py
@@ -10,10 +10,10 @@ with open(model_path, 'rb') as f:
 
 # 사용자가 입력한 경기수와 승리경기수를 기반으로 승률을 예측합니다.
 def predict_win_rate(wins, games):
-    x = np.tile(np.array([wins, games]), (77,1))
-      # 입력값의 차원을 맞춰줍니다.
-    win_rate = model.predict(x)
+    x = np.array([wins, games])
+    win_rate = model.predict(x.reshape(1, -1))
     return win_rate[0]
+
 
 # Streamlit 앱을 구성합니다.
 def main():
