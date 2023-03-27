3282079dc03e5e8c59217d8736c02c4c1d92850e
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-27 09:22:26 +0900

    ㅇㅇ

--- MH/demo.py
+++ MH/demo.py
@@ -1,79 +1,34 @@
-# import pandas as pd
-# import streamlit as st
-
-# # 데이터프레임을 불러옵니다.
-# df = pd.read_csv("https://raw.githubusercontent.com/Myun9hyun/trash/main/MH/Basketball_processing.csv")
-
-# # 체크박스로 선택한 columns 받기
-# selected_columns = st.multiselect('Select columns', df.columns)
-
-# # 선택한 columns에 해당하는 index 추출하기
-# selected_index = df[df[selected_columns].notna().all(axis=1)].index
-
-# # 선택한 index들을 출력할 데이터프레임 만들기
-# selected_df = df.loc[selected_index, selected_columns]
-
-# # 선택한 index들을 출력
-# st.write(selected_df)
 import streamlit as st
 import pandas as pd
-import plotly.graph_objects as go
-import numpy as np
-# 데이터 불러오기
-df = pd.read_csv("https://raw.githubusercontent.com/Myun9hyun/trash/main/MH/Basketball_processing.csv")
 
-# 체크박스로 선택한 columns 받기
-selected_columns = st.multiselect('Select columns', df.columns)
+# 파일 업로드
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
-# 선택한 columns에 해당하는 index 추출하기
-selected_index = df[df[selected_columns].notna().all(axis=1)].index
+if uploaded_file is not None:
+    # csv 파일을 데이터프레임으로 변환
+    df = pd.read_csv(uploaded_file)
 
-# 선택한 index들을 출력할 데이터프레임 만들기
-selected_df = df.loc[selected_index, selected_columns]
+    # CONF 열 입력 받기
+    conf = st.text_input("Enter a value for CONF column")
+    if conf != "":
+        # 입력받은 CONF 값과 일치하는 모든 행 추출
+        df = df[df["CONF"] == conf]
 
+    # YEAR 열 입력 받기
+    year = st.text_input("Enter a value for YEAR column")
+    if year != "":
+        # 입력받은 YEAR 값과 일치하는 모든 행 추출
+        df = df[df["YEAR"] == year]
 
-# 해당 컬럼에 결측값이 있을 경우 0으로 대체
-selected_df[col] = selected_df[col].fillna(0)
+    # 선택받은 columns 출력
+    columns = st.multiselect("Select columns", df.columns)
+    if len(columns) > 0:
+        df = df[columns]
 
-# 선택한 스탯들을 비교하는 radar chart 그리기
-fig = go.Figure()
-for col in selected_stats:
-    fig.add_trace(go.Scatterpolar(
-        r=[selected_df[col].mean()],
-        theta=[col],
-        fill='toself',
-        name=col
-    ))
-fig.update_layout(
-    polar=dict(
-        radialaxis=dict(
-            visible=True,
-            range=[selected_df[selected_stats].min().min(), selected_df[selected_stats].max().max()]
-        )
-    ),
-    showlegend=True
-)
-st.plotly_chart(fig, use_container_width=True)
-
-# # radar chart 그리기
-# if not selected_df.empty:
-#     fig = go.Figure()
-#     for col in selected_columns:
-#         fig.add_trace(go.Scatterpolar(
-#             r=[selected_df[col].mean()],
-#             theta=[col],
-#             fill='toself',
-#             name=col
-#         ))
-#     fig.update_layout(
-#         polar=dict(
-#             radialaxis=dict(
-#                 visible=True,
-#                 range=[selected_df[selected_columns].min().min(), selected_df[selected_columns].max().max()]
-#             )
-#         ),
-#         showlegend=True
-#     )
-#     st.plotly_chart(fig, use_container_width=True)
-# else:
-#     st.write("No data selected.")
+    # radar chart로 출력
+    if len(df) > 0:
+        st.write(df)
+        st.write("Radar Chart")
+        st.line_chart(df)
+    else:
+        st.write("No data available")
