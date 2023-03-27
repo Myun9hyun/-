79f793fb8fbb7601322a08457ec5b06f7d09c730
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-27 09:40:06 +0900

    ㅇㅇ

--- MH/demo.py
+++ MH/demo.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 # CSV 파일 업로드
 uploaded_file = st.file_uploader("Choose a file")
