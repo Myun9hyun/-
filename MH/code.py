6d34700a5658cc24f22016d97fe9d504b5d39c6f
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-27 16:14:20 +0900

    ㅇㅇ

--- MH/code.py
+++ MH/code.py
@@ -4,8 +4,6 @@ import streamlit as st
 
 # 결정트리 모델 불러오기
 model_path = "MH/DecisionTree.pkl"
-# with open(model_path, 'rb') as f:
-#     model = pickle.load(f)
 model = joblib.load(model_path)
 
 # Streamlit 앱 설정
@@ -17,15 +15,10 @@ x1 = st.slider('X1', 0.0, 1.0, 0.5, 0.01)
 x2 = st.slider('X2', 0.0, 1.0, 0.5, 0.01)
 
 # 모델을 사용하여 예측 수행
-x = np.array([x1, x2]*77).reshape(1, -1)
-# x = np.array([x1]*77).reshape(1, -1)  # 입력값의 차원을 맞춰줍니다.
+x = np.array([x1, x2] * 77).reshape(1, -1)
 y = model.predict(x)
-y = y * 100
-y = y.round(2)
-# st.write('# model')
-# st.write(model)
-y = model.predict(x)[0]
+y = y[0]
 
 # 예측 결과 출력
 st.subheader('예측 결과')
-st.write('Y:', y)
+st.write('Y:', round(y, 2))
