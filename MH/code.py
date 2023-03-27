4122474a952308bb6e0f20a1b15655d68334c2e2
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-27 16:09:18 +0900

    dd

--- MH/code.py
+++ MH/code.py
@@ -18,7 +18,7 @@ x2 = st.slider('X2', 0.0, 1.0, 0.5, 0.01)
 
 # 모델을 사용하여 예측 수행
 x = np.array([x1, x2]).reshape(1, -1)
-x = np.array([x]*154).reshape(1, -1)  # 입력값의 차원을 맞춰줍니다.
+x = np.array([x]*77).reshape(1, -1)  # 입력값의 차원을 맞춰줍니다.
 y = model.predict(x)
 y = y * 100
 y = y.round(2)
