74728016e351ea16c0afe199128167179c39173f
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-27 16:47:07 +0900

    dd

--- MH/practice.py
+++ MH/practice.py
@@ -288,47 +288,29 @@ elif choice == "데이터페이지":
                 st.write('Y:', y[0])
         elif option == 'Chart2':
 
-            # 모델 불러오기
-            model_path = "MH/model_LR.pkl"
-            with open(model_path, 'rb') as f:
-                model = joblib.load(f)
+            # 랜덤 포레스트 모델 불러오기
+            model_path = "MH/RFmodel.pkl"
+            model = joblib.load(model_path)
 
-            # 입력된 데이터를 이용해 타겟 변수를 예측하는 함수를 정의합니다.
-            def predict(model, input_df):
-                predictions = model.predict(input_df)
-                return predictions
+            # Streamlit 앱 설정
+            st.title('Random Forest Model')
+            st.write('입력 변수')
 
-            # Streamlit 앱을 정의합니다.
-            def app():
-                # 앱 제목을 설정합니다.
-                st.title("Random Forest 모델 예측")
-                
-                # 데이터 업로드를 위한 사이드바를 만듭니다.
-                st.sidebar.title("데이터 업로드")
-                uploaded_file = st.sidebar.file_uploader("CSV 파일 선택", type="csv")
-                
-                # 사용자 입력 폼을 생성합니다.
-                st.sidebar.title("입력 특성")
-                sepal_length = st.sidebar.slider("꽃받침 길이", 4.0, 8.0, 5.0)
-                sepal_width = st.sidebar.slider("꽃받침 너비", 2.0, 4.5, 3.0)
-                petal_length = st.sidebar.slider("꽃잎 길이", 1.0, 7.0, 4.0)
-                petal_width = st.sidebar.slider("꽃잎 너비", 0.1, 2.5, 1.0)
-                
-                # 사용자 입력을 데이터프레임으로 결합합니다.
-                input_data = {'sepal_length': sepal_length,
-                            'sepal_width': sepal_width,
-                            'petal_length': petal_length,
-                            'petal_width': petal_width}
-                input_df = pd.DataFrame([input_data])
-                
-                # 모델을 이용해 예측합니다.
-                if st.sidebar.button("예측"):
-                    predictions = predict(model, input_df)
-                    st.write("예측된 타겟 변수 값은:", predictions[0])
-                
-            # Streamlit 앱을 실행합니다.
-            # if __name__ == '__main__':
-            #     app()
+            # 입력 변수를 위한 슬라이더 추가
+            x1 = st.slider('X1', 0.0, 1.0, 0.5, 0.01)
+            x2 = st.slider('X2', 0.0, 1.0, 0.5, 0.01)
+            x3 = st.slider('X3', 0.0, 1.0, 0.5, 0.01)
+            x4 = st.slider('X4', 0.0, 1.0, 0.5, 0.01)
+
+            # 모델을 사용하여 예측 수행
+            x = np.array([x1, x2, x3, x4] * 19 + [x4]).reshape(1, -1)
+
+            y = model.predict(x)[0]
+
+            # 예측 결과 출력
+            st.subheader('예측 결과')
+            st.write('Y:', y)
+
         elif option == 'Chart3':
 
             # 결정트리 모델 불러오기
