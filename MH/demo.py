1de98bd40e945cce15a2fe801ccaefa8dc529016
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-27 09:35:06 +0900

    ㅇㅇ

--- MH/demo.py
+++ MH/demo.py
@@ -1,54 +1,42 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import plotly.graph_objects as go
 
-# 데이터프레임을 업로드하는 함수
-@st.cache
-def load_data(file):
-    df = pd.read_csv(file)
-    return df
+# CSV 파일 업로드
+uploaded_file = st.file_uploader("Choose a file")
 
-# 필요한 columns들만 추출하는 함수
-def filter_dataframe(df, conf, year):
-    # CONF를 기반으로 데이터프레임 필터링
-    df = df[df['CONF'] == conf]
+# CSV 파일이 업로드되었는지 확인
+if uploaded_file is not None:
+    # 업로드한 CSV 파일을 데이터프레임으로 변환
+    df = pd.read_csv(uploaded_file)
 
-    # YEAR를 기반으로 데이터프레임 필터링
-    df = df[df['YEAR'] == year]
+    # 선택한 컬럼명 받기
+    conf_col = st.selectbox("Select CONF column", options=df.columns)
+    year_col = st.selectbox("Select YEAR column", options=df.columns)
 
-    # 선택된 columns들만 추출
-    columns_to_show = st.multiselect('Select columns to show:', df.columns.tolist())
-    df = df[columns_to_show]
+    # 선택한 컬럼명으로 데이터프레임 필터링
+    conf_val = st.selectbox("Select value in CONF column", options=df[conf_col].unique())
+    year_val = st.selectbox("Select value in YEAR column", options=df[year_col].unique())
+    filtered_df = df[(df[conf_col] == conf_val) & (df[year_col] == year_val)]
 
-    return df
+    # 선택한 컬럼명으로 데이터프레임 필터링하여 multiselect로 출력할 컬럼 선택
+    select_cols = st.multiselect("Select columns to display", options=filtered_df.columns)
 
-# radar chart를 생성하는 함수
-def create_radar_chart(df, stats):
-    fig = px.line_polar(df, r=stats, theta='TEAM', line_close=True)
-    fig.update_traces(fill='toself')
+    # 선택한 컬럼만 출력
+    st.write(filtered_df[select_cols])
+
+    # TEAM의 컬럼명으로 데이터프레임 필터링하여 radar chart 출력
+    team_col = "TEAM"
+    team_val = st.selectbox("Select value in TEAM column for radar chart", options=filtered_df[team_col].unique())
+    team_df = filtered_df[filtered_df[team_col] == team_val]
+    stats = st.multiselect('Select statistics for radar chart:', filtered_df.columns.tolist())
+    theta = stats + [stats[0]]
+    fig = go.Figure(data=go.Scatterpolar(
+        r=team_df[stats].values.tolist()[0] + [team_df[stats].values.tolist()[0][0]],
+        theta=theta,
+        fill='toself'
+    ))
+    fig.update_layout(polar=dict(radialaxis=dict(visible=True, range=[0, 40])))
     st.plotly_chart(fig)
-
-# streamlit 앱 시작
-def main():
-    st.title("Radar Chart for NCAA Basketball Teams")
-
-    # 데이터프레임 업로드
-    file = st.file_uploader("Upload a CSV file", type=["csv"])
-    if file is not None:
-        df = load_data(file)
-
-        # CONF와 YEAR를 선택
-        conf = st.sidebar.selectbox('Select conference:', df['CONF'].unique())
-        year = st.sidebar.selectbox('Select year:', df['YEAR'].unique())
-
-        # 데이터프레임 필터링
-        filtered_df = filter_dataframe(df, conf, year)
-
-        # radar chart 생성
-        if 'TEAM' in filtered_df.columns:
-            stats = st.multiselect('Select statistics for radar chart:', filtered_df.columns.tolist())
-            create_radar_chart(filtered_df, stats)
-
-# 앱 실행
-if __name__ == '__main__':
-    main()
+else:
+    st.warning("Please upload a file.")
