e87a5b3f3b4a115db232e82671b675bd8bd532ac
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-27 10:25:12 +0900

    ㅇㅇ

--- MH/demo.py
+++ MH/demo.py
@@ -7,17 +7,51 @@ model_path = "MH/model.pkl"
 with open(model_path, 'rb') as f:
     model = pickle.load(f)
 
-# 예측 함수 정의
-def predict(data):
-    result = model.predict(data)
-    return result
+st.title('Linear Regression Model')
 
-# Streamlit 앱 구성
-def main():
-    st.title("머신러닝 모델 예측")
-    data = st.text_input("데이터를 입력하세요")
-    prediction = predict(data)
-    st.write("결과:", prediction)
+# create sidebar with input parameters
+st.sidebar.header('Input Parameters')
+x = st.sidebar.slider('X', 0.0, 10.0, 5.0, 0.1)
 
-if __name__ == '__main__':
-    main()
+# use model to make prediction
+y = model.predict([[x]])
+
+# show prediction result
+st.subheader('Prediction Result')
+st.write('Y:', y[0])
+
+# # 예측 함수 정의
+# def predict(data):
+#     result = model.predict(data)
+#     return result
+
+# # Streamlit 앱 구성
+# def main():
+#     st.title("머신러닝 모델 예측")
+#     data = st.text_input("데이터를 입력하세요")
+#     prediction = predict(data)
+#     st.write("결과:", prediction)
+
+# if __name__ == '__main__':
+#     main()
+
+
+# import streamlit as st
+# import pickle
+
+# # load saved linear regression model
+# with open('model.pkl', 'rb') as f:
+#     model = pickle.load(f)
+
+# st.title('Linear Regression Model')
+
+# # create sidebar with input parameters
+# st.sidebar.header('Input Parameters')
+# x = st.sidebar.slider('X', 0.0, 10.0, 5.0, 0.1)
+
+# # use model to make prediction
+# y = model.predict([[x]])
+
+# # show prediction result
+# st.subheader('Prediction Result')
+# st.write('Y:', y[0])
