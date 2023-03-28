b3cfeb20835b8f4c656fd314f881f27a7251c980
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-28 14:12:51 +0900

    dd

--- MH/code.py
+++ MH/code.py
@@ -8,12 +8,13 @@ with open('MH/DecisionTree.pkl', 'rb') as f:
 
 # 입력받기
 st.sidebar.title('Input')
-games_played = st.sidebar.slider('Number of Games Played', 0, 40, 20)
-wins = st.sidebar.slider('Number of Wins', 0, 30, 10)
-
-# 예측
 df = pd.read_csv('MH/cbb_preprocess.csv')
-data = {'G': df['G'], 'W': df['W']}
+G = st.sidebar.slider('Number of Games Played', 0, 40, 20)
+W = st.sidebar.slider('Number of Wins', 0, 40, 10)
+X_DT = df.drop('P_V', axis=1) # 독립변수 (관측값, 피쳐)
+y_DT = df['P_V']
+# 예측
+data = {X_DT, y_DT}
 df2 = pd.DataFrame(data)
 prediction = model.predict(df2)[0]
 
