54033ca295a257518cf75c5a97f0aef0df6009bc
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-28 12:06:19 +0900

    dd

--- MH/demo.py
+++ MH/demo.py
@@ -334,7 +334,7 @@ elif choice == "데이터페이지":
             # x4 = st.slider('X4', 0.0, 1.0, 0.5, 0.01)
 
             # 모델을 사용하여 예측 수행
-            x = np.array([x1, x2] * 38 + [x4]).reshape(1, -1)
+            x = np.array([x1, x2] * 38 + [x2]).reshape(1, -1)
 
             y = model.predict(x)[0]
 
