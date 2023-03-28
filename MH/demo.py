ac42d8b54647c4c8795e0ed90ec6824dcb9f2a2c
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-28 12:05:59 +0900

    dd

--- MH/demo.py
+++ MH/demo.py
@@ -321,7 +321,8 @@ elif choice == "데이터페이지":
             # 랜덤 포레스트 모델 불러오기
             model_path = "MH/RFmodel.pkl"
             model = joblib.load(model_path)
-
+            df = pd.read_csv('MH/cbb_preprocess.csv')
+            X = df.drop('P_V', axis=1) # 독립변수 (관측값, 피쳐)
             # Streamlit 앱 설정
             st.title('Random Forest Model')
             st.write('입력 변수')
@@ -329,11 +330,11 @@ elif choice == "데이터페이지":
             # 입력 변수를 위한 슬라이더 추가
             x1 = st.slider('X1', 0.0, 1.0, 0.5, 0.01)
             x2 = st.slider('X2', 0.0, 1.0, 0.5, 0.01)
-            x3 = st.slider('X3', 0.0, 1.0, 0.5, 0.01)
-            x4 = st.slider('X4', 0.0, 1.0, 0.5, 0.01)
+            # x3 = st.slider('X3', 0.0, 1.0, 0.5, 0.01)
+            # x4 = st.slider('X4', 0.0, 1.0, 0.5, 0.01)
 
             # 모델을 사용하여 예측 수행
-            x = np.array([x1, x2, x3, x4] * 19 + [x4]).reshape(1, -1)
+            x = np.array([x1, x2] * 38 + [x4]).reshape(1, -1)
 
             y = model.predict(x)[0]
 
