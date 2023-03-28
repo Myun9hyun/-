5829d18ed423cab3741a55fe9d1362c328daf55e
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-28 16:11:12 +0900

    ㅇㅇ

--- MH/code.py
+++ MH/code.py
@@ -3,22 +3,12 @@ import pandas as pd
 import joblib
 
 # 모델 불러오기
-with open('MH/DecisionTree.pkl', 'rb') as f:
-    model = joblib.load(f)
-
-# 입력받기
-st.sidebar.title('Input')
-df = pd.read_csv('MH/cbb_GW.csv')
-G = st.sidebar.slider('Number of Games Played', 0, 40, 20)
-W = st.sidebar.slider('Number of Wins', 0, 40, 10)
-# X_DT = df.drop('P_V', axis=1) # 독립변수 (관측값, 피쳐)
-X_new = pd.DataFrame({'GP': [G], 'W': [W]})
-# X_DT = X_DT.append(X_new, ignore_index=True).reset_index(drop=True)
-df.replace([np.inf, -np.inf], np.nan, inplace=True)
-df.dropna(inplace=True)
-# 예측
-prediction = model.predict(X_DT.iloc[-1].values.reshape(1, -1))[0]
-
-# 결과 출력
-st.title('Winning Percentage Prediction')
-st.write('The predicted winning percentage is:', prediction)
+with st.sidebar:
+    choose = option_menu("App Gallery", ["About", "Photo Editing", "Project Planning"],
+                         icons=['house', 'camera fill', 'kanban'],
+                         menu_icon="app-indicator", default_index=0,
+                         styles={
+        "container": {"padding": "5!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "#02ab21"},
