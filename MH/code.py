35c8b2887b480046f2ed961694cac280f3e66840
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-28 16:53:39 +0900

    d

--- MH/code.py
+++ MH/code.py
@@ -1,41 +1,16 @@
-# 필요한 라이브러리 import
-import joblib
-import numpy as np
-import pandas as pd
 import streamlit as st
-from streamlit_option_menu import option_menu
-import plotly.express as px
+import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
+import joblib
 
-# ##################################################################################
-# # st.markdown을 통해 전체 틀 고정
-# st.markdown(
-#         f"""
-# <style>
-#     .reportview-container .main .block-container{{
-#         max-width: 90%;
-#         padding-top: 5rem;
-#         padding-right: 5rem;
-#         padding-left: 5rem;
-#         padding-bottom: 5rem;
-#     }}
-#     img{{
-#     	max-width:40%;
-#     	margin-bottom:40px;
-#     }}
-# </style>
-# """,
-#         unsafe_allow_html=True,
-#     )
-# ##################################################################################
-with st.sidebar:
-    choose = option_menu("Contents", ["메인페이지", "데이터페이지", "시뮬레이션"],
-                         icons=['house', 'kanban', 'bi bi-robot'],
-                         menu_icon="app-indicator", default_index=0,
-                         styles={
-        "container": {"padding": "4!important", "background-color": "#fafafa"},
-        "icon": {"color": "black", "font-size": "25px"}, 
-        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#fafafa"},
-        "nav-link-selected": {"background-color": "#02ab21"},
-    }
-    )
+model = joblib.load("MH/Decision_drop.pkl")
+
+X = pd.read_csv("cbb_drop.csv")
+y = X.pop("target")
+
+feature_importances = pd.Series(model.feature_importances_, index=X.columns)
+
+plt.figure(figsize=(12, 10))
+sns.heatmap(df.corr(), annot=True, cmap="coolwarm")
+st.pyplot()
