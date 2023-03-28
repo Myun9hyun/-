841b17fb9daaf97820737f3868bf55d2df769ccf
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-28 16:58:17 +0900

    dd

--- MH/code.py
+++ MH/code.py
@@ -7,12 +7,12 @@ import joblib
 model = joblib.load("MH/DecisionTree_drop.pkl")
 
 X = pd.read_csv("MH/cbb_drop.csv")
-y = X.pop("target")
+y = X.pop("P_V")
 
 feature_importances = pd.Series(model.feature_importances_, index=X.columns)
 
 plt.figure(figsize=(12, 10))
-# sns.heatmap(df.corr(), annot=True, cmap="coolwarm")
-sns.heatmap(X.iloc[:, sorted_idx].corr(), cmap='coolwarm', annot=True)
+sns.heatmap(df.corr(), annot=True, cmap="coolwarm")
+# sns.heatmap(X.iloc[:, sorted_idx].corr(), cmap='coolwarm', annot=True)
 
 st.pyplot()
