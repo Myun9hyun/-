6122dcfb086d4ae1b9ccdff1af21cdf78cd4ba42
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-28 09:37:01 +0900

    ff

--- MH/code.py
+++ MH/code.py
@@ -8,8 +8,8 @@ from sklearn.linear_model import LinearRegression
 df = pd.read_csv('MH/cbb.csv')
 
 # x, y 변수 선택
-x = df['x']
-y = df['y']
+x = df['G']
+y = df['W']
 
 # 모델 훈련
 model = LinearRegression()
@@ -20,12 +20,12 @@ sns.set_style('darkgrid')
 plt.figure(figsize=(8, 6))
 sns.scatterplot(x=x, y=y)
 sns.lineplot(x=x, y=model.predict(x.values.reshape(-1, 1)))
-plt.xlabel('x')
-plt.ylabel('y')
+plt.xlabel('G')
+plt.ylabel('W')
 plt.title('Linear Regression')
 st.pyplot()
 
 # 모델 예측
-x_new = st.slider('x', min_value=0, max_value=10)
+x_new = st.slider('G', min_value=0, max_value=10)
 y_new = model.predict([[x_new]])[0]
 st.write(f'예측 결과: {y_new:.2f}')
