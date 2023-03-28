485e73467ed0b41630f8b0f5ff48bee1dc22e80a
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-28 10:00:38 +0900

    dd

--- MH/code.py
+++ MH/code.py
@@ -3,6 +3,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import joblib
+import numpy as np
 
 # 모델 로드
 model = joblib.load('MH/LRmodel.pkl')
@@ -11,16 +12,17 @@ model = joblib.load('MH/LRmodel.pkl')
 df = pd.read_csv('MH/cbb_preprocess.csv')
 
 # x 값 생성
-x = df['G']
+x = df.drop('P_V', axis=1).values
 
 # 예측값 생성
-y_pred = model.predict(x.values.reshape(-1, 1))
+x_77d = np.hstack([x, np.zeros((len(x), 77 - len(x[0])), dtype=x.dtype)])
+y_pred = model.predict(x_77d)
 
 # 그래프 그리기
 sns.set_style('darkgrid')
 plt.figure(figsize=(8, 6))
-sns.scatterplot(x=x, y=df['P_V'])
-sns.lineplot(x=x, y=y_pred)
+sns.scatterplot(x=df['G'], y=df['P_V'])
+sns.lineplot(x=df['G'], y=y_pred)
 plt.xlabel('G')
 plt.ylabel('P_V')
 plt.title('Linear Regression')
