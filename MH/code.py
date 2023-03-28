c855ec8faa9aaba3c11c21dd22885cb84e286295
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-28 10:56:06 +0900

    dd

--- MH/code.py
+++ MH/code.py
@@ -2,31 +2,24 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
+import joblib
 
-# Load the data
+# 데이터 불러오기
 df = pd.read_csv('MH/cbb_preprocess.csv')
 
-# Train the linear regression model
-# X = df['P_V'].values.reshape(-1, 1)
-# y = df['P_V'].values
-X = df.drop('P_V', axis=1).values.reshape(-1, 1) # 독립변수 (관측값, 피쳐)
-y = df['P_V'].values # 종속변수 (예측값, 라벨)
-lr = LinearRegression()
-lr.fit(X, y)
+# 모델 불러오기
+with open('MH/LRmodel.pkl', 'rb') as f:
+    model = joblib.load(f)
 
-# Draw the scatter plot
+# 예측값 계산
+df['predicted'] = model.predict(df[['feature1', 'feature2', 'feature3']])
+
+# 산점도 그리기
 sns.set_style('darkgrid')
 plt.figure(figsize=(8, 6))
-plt.xlabel('G')
-plt.ylabel('P_V')
+plt.xlabel('feature1')
+plt.ylabel('predicted')
 plt.title('Linear Regression')
-plt.scatter(X, y)
 
-# Draw the regression line
-x_range = range(df['G'].min(), df['G'].max() + 1)
-y_range = lr.predict([[x] for x in x_range])
-plt.plot(x_range, y_range, c='red')
-
-# Display the plot using Streamlit
+plt.scatter(df['feature1'], df['predicted'])
 st.pyplot()
