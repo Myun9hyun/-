d0632290f8f174aba2f36eb1349f90a85d9e199e
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-28 10:49:16 +0900

    dd

--- MH/code.py
+++ MH/code.py
@@ -8,8 +8,10 @@ from sklearn.linear_model import LinearRegression
 df = pd.read_csv('MH/cbb_preprocess.csv')
 
 # Train the linear regression model
-X = df['G'].values.reshape(-1, 1)
-y = df['P_V'].values
+# X = df['P_V'].values.reshape(-1, 1)
+# y = df['P_V'].values
+X = df.drop('P_V', axis=1).values.reshape(-1, 1) # 독립변수 (관측값, 피쳐)
+y = df['P_V'].values # 종속변수 (예측값, 라벨)
 lr = LinearRegression()
 lr.fit(X, y)
 
