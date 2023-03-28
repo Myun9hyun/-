c3c320c53a423750ed3fe64dfdf3a19e9f62ecda
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-28 12:03:01 +0900

    dd

--- MH/demo.py
+++ MH/demo.py
@@ -310,9 +310,9 @@ elif choice == "데이터페이지":
 
             if predict_button:
                     predicted = model.predict(X)
-                    # variable1 = np.array([승리수, 경기수] * 28)
+                    variable1 = np.array([승리수, 경기수] * 28)
                     model1 = joblib.load('MH/LRmodel.pkl')
-                    pred1 = model1.predict([predicted])
+                    # pred1 = model1.predict([predicted])
                     pred1 = pred1.round(2)
                     st.metric("결과: ", pred1[0])
 
