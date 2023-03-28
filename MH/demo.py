561b019d97e66564e05ebc8da02e020c23b8c649
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-28 16:26:54 +0900

    dd

--- MH/demo.py
+++ MH/demo.py
@@ -13,17 +13,8 @@ import seaborn as sns
 from streamlit_option_menu import option_menu
 
 
-with st.sidebar:
-    choice = option_menu("Contents", ["메인페이지", "데이터페이지", "시뮬레이션"],
-                         icons=['house', 'kanban', 'bi bi-robot'],
-                         menu_icon="app-indicator", default_index=0,
-                         styles={
-        "container": {"padding": "4!important", "background-color": "#fafafa"},
-        "icon": {"color": "black", "font-size": "25px"}, 
-        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#fafafa"},
-        "nav-link-selected": {"background-color": "#02ab21"},
-    }
-    )
+menu = ["메인페이지", "데이터페이지", "시뮬레이션"]
+choice = st.sidebar.selectbox("메뉴를 선택해주세요", menu)
 
 if choice == "메인페이지":
 
