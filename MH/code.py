41938c4080eebebb1e8358a8e4e886c3ebdd607c
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-28 10:04:29 +0900

    dd

--- MH/code.py
+++ MH/code.py
@@ -2,28 +2,28 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-import joblib
-import numpy as np
+import pickle
 
-# 모델 로드
-model = joblib.load('MH/LRmodel.pkl')
+# 모델 불러오기
+with open('MH/LRmodel.pkl', 'rb') as f:
+    model = pickle.load(f)
 
-# 데이터 로드
-df = pd.read_csv('MH/cbb_preprocess.csv')
-
-# x 값 생성
-x = df.drop('P_V', axis=1).values
-
-# 예측값 생성
-x_56d = np.hstack([x, np.zeros((len(x), 56 - len(x[0])), dtype=x.dtype)])
-y_pred = model.predict(x_56d)
-
-# 그래프 그리기
+# 산점도 그리기
 sns.set_style('darkgrid')
 plt.figure(figsize=(8, 6))
-sns.scatterplot(x=df['G'], y=df['P_V'])
-sns.lineplot(x=df['G'], y=y_pred)
 plt.xlabel('G')
 plt.ylabel('P_V')
 plt.title('Linear Regression')
+
+# 모델 예측 및 그래프 그리기
+x_min = 0
+x_max = 40
+x_new = st.slider('G', min_value=x_min, max_value=x_max)
+y_new = model.predict([[x_new]])[0]
+
+x = list(range(x_min, x_max+1))
+y = model.predict([[i] for i in x])
+
+plt.plot(x, y)
+plt.scatter(x_new, y_new, c='red')
 st.pyplot()
