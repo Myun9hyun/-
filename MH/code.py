3fcf9bc5b047c49c9cfee40afb19bb27c1c6cba8
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-28 11:15:59 +0900

    ㅇㅇ

--- MH/code.py
+++ MH/code.py
@@ -7,9 +7,6 @@ import joblib
 # 데이터 불러오기
 df = pd.read_csv('MH/cbb_preprocess.csv')
 X = df.drop('P_V', axis=1) # 독립변수 (관측값, 피쳐)
-# st.write(X.isna().sum())
-# st.write(len(X.columns))
-# y = df['P_V'] # 종속변수 (예측값, 라벨)
 
 # 모델 불러오기
 with open('MH/LRmodel.pkl', 'rb') as f:
@@ -21,10 +18,7 @@ df['predicted'] = model.predict(X)
 # 산점도 그리기
 sns.set_style('darkgrid')
 plt.figure(figsize=(8, 6))
-# plt.xlabel('P_V')
-# plt.ylabel('predicted')
-# plt.title('Linear Regression')
+plt.title('Linear Regression')
 
-# sns.scatterplot(df['P_V'], df['predicted'])
 sns.scatterplot(x = 'P_V', y='predicted', data=df)
 st.pyplot()
