bd8a765cf57259e80f53d4633708a204acb609c3
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-28 12:09:21 +0900

    dd

--- MH/demo.py
+++ MH/demo.py
@@ -322,8 +322,8 @@ elif choice == "데이터페이지":
             model_path = "MH/RFmodel.pkl"
             model = joblib.load(model_path)
             df = pd.read_csv('MH/cbb_preprocess.csv')
-            X_RF = bd.drop('Winning_rate', axis=1)
-            y_RF = bd['Winning_rate']
+            X_RF = df.drop('Winning_rate', axis=1)
+            y_RF = df['Winning_rate']
             # Streamlit 앱 설정
             st.title('Random Forest Model')
             st.write('입력 변수')
