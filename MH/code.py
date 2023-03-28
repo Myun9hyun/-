eef85b29ba80cf7aa0ba6d7b6d20ff2c074272a1
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-28 09:58:56 +0900

    dd

--- MH/code.py
+++ MH/code.py
@@ -2,30 +2,26 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
+import joblib
+
+# 모델 로드
+model = joblib.load('MH/LRmodel.pkl')
 
 # 데이터 로드
 df = pd.read_csv('MH/cbb_preprocess.csv')
 
-# x, y 변수 선택
-x = df['G'] # 독립변수 (관측값, 피쳐)
-y = df['P_V']
+# x 값 생성
+x = df['G']
 
-# 모델 훈련
-model = LinearRegression()
-model.fit(x.values.reshape(-1, 1), y)
+# 예측값 생성
+y_pred = model.predict(x.values.reshape(-1, 1))
 
-# 산점도 그리기
+# 그래프 그리기
 sns.set_style('darkgrid')
-fig, ax = plt.subplots(figsize=(8, 6))
-sns.scatterplot(x=x, y=y, ax=ax)
-sns.lineplot(x=x, y=model.predict(x.values.reshape(-1, 1)), ax=ax)
+plt.figure(figsize=(8, 6))
+sns.scatterplot(x=x, y=df['P_V'])
+sns.lineplot(x=x, y=y_pred)
 plt.xlabel('G')
-plt.ylabel('W')
+plt.ylabel('P_V')
 plt.title('Linear Regression')
-st.pyplot(fig)
-
-# 모델 예측
-x_new = st.slider('P_V', min_value=0, max_value=40)
-y_new = model.predict([[x_new]])[0]
-st.write(f'예측 결과: {y_new:.2f}')
+st.pyplot()
