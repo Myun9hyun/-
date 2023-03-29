8de44c7b88313bd41e428154d10f30c5c6af0f15
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-29 17:22:54 +0900

    ㅇㅇ

--- MH/demo.py
+++ MH/demo.py
@@ -13,8 +13,9 @@ def random_values(values, probabilities, n):
 st.title("Random Value Generator")
 
 # 값과 그에 해당하는 확률을 리스트로 지정합니다.
-values = ['Cozem', 'Coupon', 'Epic', 'Cube', 'Fire']
-probabilities = [0.1, 0.2, 0.3, 0.3, 0.1]
+values = ['코젬', '경뿌', '반파별4개', '수에큐3개', '소경축비', '명큡', '앱상', '강환불', '미코젬', '주흔묶음', '반빨', '재획비', '경축비', '고보킬', '고대비', '명훈', '장큐', '거코젬']
+probabilities = [0.074, 0.074, 0.074, 0.074, 0.074, 0.03, 0.03, 0.03, 0.03, 0.03, 0.06, 0.06, 0.06, 0.06, 0.06, 0.06, 0.06, 0.06]
+
 
 # 출력을 원하는 개수를 입력받습니다.
 n = st.number_input("출력을 원하는 개수를 입력하세요:", min_value=1, max_value=10, step=1, value=1)
