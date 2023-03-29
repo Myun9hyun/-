2f0985df5de15d0c201dab1e90a11a8d38cfa98e
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-29 17:57:00 +0900

    ㅇㅇ

--- MH/demo.py
+++ MH/demo.py
@@ -34,21 +34,33 @@ n = st.number_input("상자를 오픈하실 개수를 입력하세요:", min_val
 # 값을 랜덤하게 선택하여 출력합니다.
 # selected_values = random_values(values, probabilities,n)
 
+# open_button = st.button("상자 열기")
+# if open_button:
+    
+#     selected_values = random_values(values, probabilities, n)
+#     if values == '코젬' or '경뿌' or '반파별4개' or'수에큐3개' or '소경축비':
+#         for i in range(min(n, len(selected_values))):
+#             st.write(f"축하드립니다! {selected_values[i]}(이)가 당첨되었습니다!")
+
+#     elif values == '명큡' or '앱상' or'강환불' or'미코젬' or '주흔_한묶음':
+#         st.balloons()
+#         for i in range(min(n, len(selected_values))):
+            
+#             st.write(f"축하드립니다! {selected_values[i]}(이)가 당첨되었습니다!")
+
+#     elif values == '반빨별' or '재획비' or'경축비' or '고보킬' or '고대비' or '명훈' or '장큐' or '거코젬':
+#         for i in range(min(n, len(selected_values))):
+#             st.write(f"축하드립니다! {selected_values[i]}(이)가 당첨되었습니다!")
 open_button = st.button("상자 열기")
 if open_button:
-    
     selected_values = random_values(values, probabilities, n)
-    if values == '코젬' or '경뿌' or '반파별4개' or'수에큐3개' or '소경축비':
-        # st.balloons()
+    if values in ('코젬', '경뿌', '반파별4개', '수에큐3개', '소경축비'):
         for i in range(min(n, len(selected_values))):
             st.write(f"축하드립니다! {selected_values[i]}(이)가 당첨되었습니다!")
-
-    elif values == '명큡' or '앱상' or'강환불' or'미코젬' or '주흔_한묶음':
+    elif values in ('명큡', '앱상', '강환불', '미코젬', '주흔_한묶음'):
         st.balloons()
         for i in range(min(n, len(selected_values))):
-            
             st.write(f"축하드립니다! {selected_values[i]}(이)가 당첨되었습니다!")
-
-    elif values == '반빨별' or '재획비' or'경축비' or '고보킬' or '고대비' or '명훈' or '장큐' or '거코젬':
+    elif values in ('반빨별', '재획비', '경축비', '고보킬', '고대비', '명훈', '장큐', '거코젬'):
         for i in range(min(n, len(selected_values))):
             st.write(f"축하드립니다! {selected_values[i]}(이)가 당첨되었습니다!")
