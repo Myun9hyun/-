0f16721651d83ac26603ea6270406ceadabc15aa
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-30 17:03:33 +0900

    ㅇㅇ

--- MH/demo.py
+++ MH/demo.py
@@ -6,19 +6,12 @@ total_apples = 10
 # 사용자 입력 처리
 with st.form(key='buy_form'):
     quantity = st.number_input('사과 갯수', min_value=1, max_value=total_apples)
-    st.form_submit_button('구매')
+    submitted = st.form_submit_button('구매')
 
 # 상태 업데이트
-if 'quantity' in st.session_state:
-    total_apples -= st.session_state['quantity']
+if submitted:
+    total_apples -= quantity
+    st.session_state.quantity = quantity
 
 # 상태 출력
 st.write(f'사과 갯수: {total_apples}')
-import streamlit as st
-
-# 초기 상태 설정
-total_apples = 10
-
-# 사용자 입력 처리
-with st.form(key='buy_form'):
-    quantity = st.number
