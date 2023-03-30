0f90ab9418286dc4c3581f75e711b4cb8aa5dc93
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-30 22:22:01 +0900

    ㅇㅇ

--- MH/demo.py
+++ MH/demo.py
@@ -1,23 +1,89 @@
 import streamlit as st
+import sqlite3
+from streamlit_SessionState import SessionState
 
-# 초기 상태 설정
-total_apples = 10
+# 페이지 넓이 설정
+st.set_page_config(page_title='온라인 상점', page_icon=':shopping_bags:', layout='wide')
 
-# 사용자 입력 처리
-with st.form(key='buy_form'):
-    quantity = st.number_input('사과 갯수', min_value=1, max_value=total_apples)
-    if st.form_submit_button('구매'):
-        st.session_state['quantity'] = quantity
+# 사이드바 설정
+st.sidebar.title('메뉴')
+selected_menu = st.sidebar.radio('', ['상품 구매', '장바구니', '주문 내역'])
 
-# 상태 업데이트
-if 'quantity' in st.session_state:
-    repeat = st.session_state['quantity']
-    for i in range(repeat):
-        if total_apples > 0:
-            total_apples -= 1
+# 데이터베이스 연결
+conn = sqlite3.connect('store.db')
+cur = conn.cursor()
 
-# 상태 출력
-if total_apples == 0:
-    st.warning('품절되었습니다.')
-else:
-    st.write(f'사과 갯수: {total_apples}')
+# 테이블 조회 함수
+def select_products():
+    cur.execute("SELECT * FROM products")
+    products = cur.fetchall()
+    return products
+
+# 테이블 업데이트 함수
+def update_product_quantity(id, quantity):
+    cur.execute("UPDATE products SET quantity = ? WHERE id = ?", (quantity, id))
+    conn.commit()
+
+# 상품 정보 표시
+def display_product_info(product, session):
+    col1, col2, col3, col4 = st.beta_columns([1, 1, 1, 0.5])
+    with col1:
+        st.write(product[0])
+    with col2:
+        st.write(product[1])
+    with col3:
+        st.write(product[2])
+    with col4:
+        if product[3] == 0:
+            st.write('품절')
+        else:
+            quantity = st.number_input('수량', value=1, min_value=1, max_value=product[3])
+            if st.button('구매'):
+                update_product_quantity(product[0], product[3]-quantity)
+                st.success(f'{product[1]} {quantity}개 구매 완료')
+
+                # 장바구니에 추가
+                if 'cart' not in session:
+                    session.cart = {}
+                if product[1] not in session.cart:
+                    session.cart[product[1]] = {'price': product[2], 'quantity': quantity}
+                else:
+                    session.cart[product[1]]['quantity'] += quantity
+
+# 장바구니 표시
+def display_cart(session):
+    st.header('장바구니')
+    total_price = 0
+    for name, item in session.cart.items():
+        st.write(f'{name} ({item["quantity"]}개): {item["price"]*item["quantity"]}원')
+        total_price += item["price"]*item["quantity"]
+    st.write(f'총 가격: {total_price}원')
+
+# 주문 내역 표시
+def display_order_history():
+    st.header('주문 내역')
+
+# 메인 함수
+def main():
+    # 로그인 세션 관리
+    session = SessionState.get(cart={})
+
+    # 상품 정보 가져오기
+    products = select_products()
+
+    # 페이지 헤더
+    st.header('온라인 상점')
+
+    # 페이지 내용
+    if selected_menu == '상품 구매':
+        for product in products:
+            display_product_info(product, session)
+
+    elif selected_menu == '장바구니':
+        display_cart(session)
+
+    elif selected_menu == '주문 내역':
+        display_order_history()
+
+if __name__ == '__main__':
+    main()
