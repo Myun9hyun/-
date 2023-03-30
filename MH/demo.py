b9cce5d857da4c05656bfc9566866afdcec364c7
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-30 17:06:19 +0900

    ㅇㅇ

--- MH/demo.py
+++ MH/demo.py
@@ -4,17 +4,14 @@ import streamlit as st
 total_apples = 10
 
 # 사용자 입력 처리
-with st.form(key='buy_form'):
-    quantity = st.number_input('사과 갯수', min_value=1, max_value=total_apples)
-    submitted = st.form_submit_button('구매')
+while total_apples > 0:
+    with st.form(key='buy_form'):
+        quantity = st.number_input('사과 갯수', min_value=1, max_value=total_apples)
+        submitted = st.form_submit_button('구매')
 
-# 상태 업데이트
-if submitted:
-    if quantity > total_apples:
-        st.error('재고보다 많은 수량을 구매할 수 없습니다.')
-    else:
+    # 상태 업데이트
+    if submitted:
         total_apples -= quantity
-        st.session_state.quantity = quantity
 
-# 상태 출력
-st.write(f'사과 갯수: {total_apples}')
+    # 상태 출력
+    st.write(f'남은 사과 갯수: {total_apples}')
