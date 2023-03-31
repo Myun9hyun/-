9964f31ec0f099a497192f4d5f23d083802c7919
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-31 21:02:34 +0900

    dd

--- MH/demo.py
+++ MH/demo.py
@@ -96,7 +96,7 @@ data = load_data()
 def delete_data(name):
     global data
     for i, item in enumerate(data):
-        if item['name'] == name:
+        if item['Name'] == name:
             del data[i]
             return True
     return False
