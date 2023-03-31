d40855c80663266e9190c9f45adba864a57ebcf2
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-31 23:31:56 +0900

    dd

--- MH/cozem.py
+++ MH/cozem.py
@@ -299,8 +299,12 @@ elif choice == "명단관리":
         
         df2_O = df2[df2['Novel'] == 'O']
         df2_X = df2[df2['Novel'] == 'X']
-        novel_up = df1_X['Name'] or df2_O['Name']
-        st.write(novel_up)
+        # df1_O에서 Name열에 해당하는 index 리스트로 추출
+        name_index = df1_O.index[df1_O['Name'].notna()].tolist()
+        st.write("Name 열 index 리스트:", name_index)
+
+        # novel_up = df1_X['Name'] or df2_O['Name']
+        # st.write(novel_up)
         st.write(df2_X.index['Name'])
         duplicates = set(df1['Name']).intersection(set(df2['Name']))
 
