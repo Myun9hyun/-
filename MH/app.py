7f5d44f6060336dc8de4468ba9df11048321181a
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-31 16:25:00 +0900

    dd

--- MH/app.py
+++ MH/app.py
@@ -31,10 +31,10 @@ def divide_cozem(cozem_sums):
 
 # 위클리 코젬 내야하는 갯수
 def manager(cozem_sums):
-    st.write(f"반디 : {divide_cozem(n)[1]} 개")
-    st.write(f"샴푸 : {divide_cozem(n)[2]} 개")
-    st.write(f"둥둥 : {divide_cozem(n)[3]} 개")
-    st.write(f"돌체 : {divide_cozem(n)[0]} 개")
+    st.write(f"반디 : {divide_cozem(cozem_sums)[1]} 개")
+    st.write(f"샴푸 : {divide_cozem(cozem_sums)[2]} 개")
+    st.write(f"둥둥 : {divide_cozem(cozem_sums)[3]} 개")
+    st.write(f"돌체 : {divide_cozem(cozem_sums)[0]} 개")
 
 def Flag_cozem(flag):
     if flag >= 0 and flag < 500:
