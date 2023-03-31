24eda52b3ac15c260edb01ff37ae38a1f69ccc18
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-31 14:38:08 +0900

    ㅇㅇ

--- MH/app.py
+++ MH/app.py
@@ -75,14 +75,33 @@ def clear_data():
 data = load_data()
 
 # 사용자로부터 이름과 점수를 입력받아 데이터프레임에 추가하는 함수
+# def add_data(name, weekly_mission, suro, flag):
+#     global data
+#     data = data.append({
+#         'Name': name, 
+#         'Weekly_Mission' : weekly_mission, 
+#         'Suro': suro, 
+#         'Flag' : flag,
+#          }, ignore_index=True)
 def add_data(name, weekly_mission, suro, flag):
     global data
+    suro_cozem = Suro_cozem(suro)  # Suro_cozem 함수로부터 반환된 값을 변수에 저장합니다.
+    flag_cozem = flag_cozem(flag)  # flag_cozem 함수로부터 반환된 값을 변수에 저장합니다.
+    cozem_total = suro_cozem + flag_cozem  # 코젬 총합을 계산합니다.
+    novel_use = novel()  # novel 함수로부터 반환된 값을 변수에 저장합니다.
+    
+    # 데이터프레임에 추가합니다.
     data = data.append({
         'Name': name, 
-        'Weekly_Mission' : weekly_mission, 
+        'Weekly_Mission': weekly_mission, 
         'Suro': suro, 
-        'Flag' : flag,
-         }, ignore_index=True)
+        'Suro_Cozem': suro_cozem,  # Suro_cozem 함수로부터 반환된 값을 추가합니다.
+        'Flag': flag, 
+        'Flag_Cozem': flag_cozem,  # flag_cozem 함수로부터 반환된 값을 추가합니다.
+        'Cozem_Total': cozem_total,  # 코젬 총합을 추가합니다.
+        'Novel': novel_use  # novel 함수로부터 반환된 값을 추가합니다.
+    }, ignore_index=True)
+
 
 # Streamlit 앱 생성
 def main():
@@ -102,8 +121,11 @@ def main():
         st.success('Data Added Successfully')
     
     # 저장된 데이터프레임 출력
+    # if st.button('Display Data'):
+    #     st.write(data)
     if st.button('Display Data'):
-        st.write(data)
+    st.write(data[['Name', 'Weekly_Mission', 'Suro', 'Suro_Cozem', 'Flag', 'Flag_Cozem', 'Cozem_Total', 'Novel']])
+
 
     # 데이터 초기화 버튼
     if st.button('Clear Data'):
