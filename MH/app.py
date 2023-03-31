51382a674d63c651b99d959579b640087ebd38fd
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-31 14:38:32 +0900

    ㅇㅇ

--- MH/app.py
+++ MH/app.py
@@ -124,7 +124,7 @@ def main():
     # if st.button('Display Data'):
     #     st.write(data)
     if st.button('Display Data'):
-    st.write(data[['Name', 'Weekly_Mission', 'Suro', 'Suro_Cozem', 'Flag', 'Flag_Cozem', 'Cozem_Total', 'Novel']])
+        st.write(data[['Name', 'Weekly_Mission', 'Suro', 'Suro_Cozem', 'Flag', 'Flag_Cozem', 'Cozem_Total', 'Novel']])
 
 
     # 데이터 초기화 버튼
