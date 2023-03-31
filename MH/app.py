1419c6813480c05b1695ea4c273d581cecaf0b85
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-31 16:20:57 +0900

    dd

--- MH/app.py
+++ MH/app.py
@@ -12,11 +12,11 @@ flags_cozem = [] # 플래그 점수에 따른 코젬 갯수 입력 리스트
 
 
 # 이번주 위클리 분배
-def divide_cozem(weekly_total):
-    cozem = weekly_total // 4  # 몫
-    cozem_else = weekly_total % 4  # 나머지
-    if weekly_total < 3:  # 입력값이 3 미만인 경우
-        return [weekly_total] + [0] * (3 - weekly_total)
+def divide_cozem(cozem_sums):
+    cozem = cozem_sums // 4  # 몫
+    cozem_else = cozem_sums % 4  # 나머지
+    if cozem_sums < 3:  # 입력값이 3 미만인 경우
+        return [cozem_sums] + [0] * (3 - cozem_sums)
     if cozem_else == 0:  # 4로 나누어 떨어지는 경우
         return [cozem] * 4
     elif cozem_else == 1:  # 4로 나누었을 때 나머지가 1인 경우
@@ -26,7 +26,7 @@ def divide_cozem(weekly_total):
     elif cozem_else == 3:  # 4으로 나누었을 때 나머지가 3인 경우
         return [cozem_else, cozem_else + 1, cozem_else + 1, cozem_else + 1]
     # 입력값이 4미만일 경우 오류 -> 해결 필요
-    n = weekly_total
+    n = cozem_sums
     
 
 # 위클리 코젬 내야하는 갯수
