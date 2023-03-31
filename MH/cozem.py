a3f75f4cb8006fece0228cc35a3e5ca5b9a303f6
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-31 23:55:40 +0900

    ㅇㅇ

--- MH/cozem.py
+++ MH/cozem.py
@@ -350,50 +350,61 @@ elif choice == "아카이브":
         st.write("봄 컨셉 포스터입니다")
         st.image("https://media.licdn.com/dms/image/D5622AQFO0CCKhf9Drg/feedshare-shrink_2048_1536/0/1679574361605?e=1682553600&v=beta&t=MX4A4NE3E-BJrCI_1-uh3LRAtKZWtpbofbB1ZKN-ykg", width=500)    
 else:
+    tab1, tab2= st.tabs(["🎁Random Box", "🔗Link"])
+    with tab1:
+        def random_values(values, probabilities, n):
+            # n번 값을 랜덤하게 선택하여 반환합니다.
+            result = []
+            for i in range(n):
+                selected_value = random.choices(values, probabilities)[0]
+                result.append(selected_value)
+            return result
+
+        # Streamlit 앱을 실행합니다.
+        st.title("🐻아기자기 랜덤박스🎁")
+        st.write()
+        '''
+        ##### 랜덤박스🎁 내 물품은 다음과 같습니다
+
+        | 구분 |  구성품 | 확률 | 
+        |:---: | :---: | :---: | 
+        | 꽝💣 | 코젬, 경뿌, 반파별4개, 수에큐3개 | 7.4% |
+        | 대박🎊 | 명큡, 앱솔상자, 강환불, 미코젬, 주흔 한묶음 | 6% |
+        | 일반💰 | 반빨별, 재획비, 경축비, 고보킬, 고대비, 명훈, 장큐, 거코젬 | 3% | 
 
 
-    def random_values(values, probabilities, n):
-        # n번 값을 랜덤하게 선택하여 반환합니다.
-        result = []
-        for i in range(n):
-            selected_value = random.choices(values, probabilities)[0]
-            result.append(selected_value)
-        return result
-
-    # Streamlit 앱을 실행합니다.
-    st.title("🐻아기자기 랜덤박스🎁")
-    st.write()
-    '''
-    ##### 랜덤박스🎁 내 물품은 다음과 같습니다
-
-    | 구분 |  구성품 | 확률 | 
-    |:---: | :---: | :---: | 
-    | 꽝💣 | 코젬, 경뿌, 반파별4개, 수에큐3개 | 7.4% |
-    | 대박🎊 | 명큡, 앱솔상자, 강환불, 미코젬, 주흔 한묶음 | 6% |
-    | 일반💰 | 반빨별, 재획비, 경축비, 고보킬, 고대비, 명훈, 장큐, 거코젬 | 3% | 
+        '''
+        # 값과 그에 해당하는 확률을 리스트로 지정합니다.
+        values = ['코젬', '경뿌', '반파별4개', '수에큐3개', '소경축비', '명큡', '앱상', '강환불', '미코젬', '주흔_한묶음', '반빨별', '재획비', '경축비', '고보킬', '고대비', '명훈', '장큐', '거코젬']
+        probabilities = [0.074, 0.074, 0.074, 0.074, 0.074, 0.03, 0.03, 0.03, 0.03, 0.03, 0.06, 0.06, 0.06, 0.06, 0.06, 0.06, 0.06, 0.06]
 
 
-    '''
-    # 값과 그에 해당하는 확률을 리스트로 지정합니다.
-    values = ['코젬', '경뿌', '반파별4개', '수에큐3개', '소경축비', '명큡', '앱상', '강환불', '미코젬', '주흔_한묶음', '반빨별', '재획비', '경축비', '고보킬', '고대비', '명훈', '장큐', '거코젬']
-    probabilities = [0.074, 0.074, 0.074, 0.074, 0.074, 0.03, 0.03, 0.03, 0.03, 0.03, 0.06, 0.06, 0.06, 0.06, 0.06, 0.06, 0.06, 0.06]
+        # 출력을 원하는 개수를 입력받습니다.
+        n = st.number_input("상자를 오픈하실 개수를 입력하세요:", min_value=1, max_value=10, step=1, value=1)
 
+        # 값을 랜덤하게 선택하여 출력합니다.
+        selected_values = random_values(values, probabilities,n)
+        # st.success('This is a success message!', icon="✅")
+        open_button = st.button("상자 열기")
+        if open_button:
+            selected_values = random_values(values, probabilities, n)
+            for i in range(min(n, len(selected_values))):
+                if selected_values[i] in ['코젬', '경뿌', '반파별4개', '수에큐3개', '소경축비']:
+                    st.error(f"아쉽습니다.. {selected_values[i]}(이)가 나왔습니다..")
+                elif selected_values[i] in ['명큡', '앱상', '강환불', '미코젬', '주흔_한묶음']:
+                    st.balloons()
+                    st.success(f"축하드립니다! 상자에서 {selected_values[i]}(이)가 나왔습니다!")
+                else:
+                    st.warning(f"상자에서 {selected_values[i]}(이)가 나왔습니다!")
 
-    # 출력을 원하는 개수를 입력받습니다.
-    n = st.number_input("상자를 오픈하실 개수를 입력하세요:", min_value=1, max_value=10, step=1, value=1)
+    with tab2:
+        tab2.subheader("🖇️ Link Tab")
+        tab2.write("각종 링크는 아래에 있습니다.")
+        st.write()
+        '''
+        ---
+        | 구분 | 이름  | 링크 | 
+        | :---: | :---: | :---: | 
+        | GoogleDocs | 📑아기자기명단 | [![Colab](https://img.shields.io/badge/GoogleDocs-아기자기명단-green)](https://onedrive.live.com/edit.aspx?resid=221CE48C87202DCA!2450&ithint=file%2cxlsx&authkey=!ADKQOeLCxzQp_5o) | 
 
-    # 값을 랜덤하게 선택하여 출력합니다.
-    selected_values = random_values(values, probabilities,n)
-    # st.success('This is a success message!', icon="✅")
-    open_button = st.button("상자 열기")
-    if open_button:
-        selected_values = random_values(values, probabilities, n)
-        for i in range(min(n, len(selected_values))):
-            if selected_values[i] in ['코젬', '경뿌', '반파별4개', '수에큐3개', '소경축비']:
-                st.error(f"아쉽습니다.. {selected_values[i]}(이)가 나왔습니다..")
-            elif selected_values[i] in ['명큡', '앱상', '강환불', '미코젬', '주흔_한묶음']:
-                st.balloons()
-                st.success(f"축하드립니다! 상자에서 {selected_values[i]}(이)가 나왔습니다!")
-            else:
-                st.warning(f"상자에서 {selected_values[i]}(이)가 나왔습니다!")
-
+        
