2b4607dd89325a4ae34215d45718ecb63087ef52
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-31 22:08:27 +0900

    ㅇㅇ

--- MH/app.py
+++ MH/app.py
@@ -149,7 +149,7 @@ def main():
         st.write(data[data['Novel'] == 'O'])
     # 다운로드 버튼 클릭
     if st.button("다운로드"):
-        file_name = st.text_input("저장할 파일명을 입력하세요:", "아기자기")
+        file_name = st.text_input("저장할 파일명을 입력하세요:")
         # file_name = st.text_input('Enter the file name:', 'my_data')
         if st.button("저장"):
             st.markdown(download_csv(data, file_name), unsafe_allow_html=True)
