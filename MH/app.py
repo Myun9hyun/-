bcfb8fe56e17e45183ee5842eff09eb4b75f9dcc
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-31 16:09:29 +0900

    dd

--- MH/app.py
+++ MH/app.py
@@ -122,7 +122,7 @@ def main():
         st.warning('Data Cleared Successfully')
 
     if st.button('Cozem sum'):
-        weekly_total = data['cozem_total']
+        weekly_total = data['Cozem_Total']
         st.write(f"{weekly_total}개")
 
 if __name__ == '__main__':
