dbc1d0e08927a08bec0988aaf1bbd6e6390dbc5a
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-31 21:43:23 +0900

    ㅇㅇ

--- MH/cozem.py
+++ MH/cozem.py
@@ -163,7 +163,7 @@ elif choice == "길드페이지":
 
 
         def main():
-            st.title('cozem')
+            
             options = ["데이터 추가", "데이터 조회", "데이터 초기화", "노블 사용or제한", "위클리 코젬 계산"]
             option = st.selectbox("기능 선택", options)
             
@@ -181,10 +181,6 @@ elif choice == "길드페이지":
                     save_data(data)  # 데이터를 파일에 저장
                     st.success('데이터가 추가되었습니다.')
 
-            # if st.button('데이터 삭제'):
-            #     name_to_delete = st.text_input('삭제할 이름을 입력하세요')
-            #     if name_to_delete in data['Name'].values:
-            #         data = data[data['Name'] != name_to_delete]
             elif option == "데이터 조회":
                 # 저장된 데이터
                 if st.button('차트 열기'):
