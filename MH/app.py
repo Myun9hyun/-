ffb899b1e6babb7e183d88facdb5f0318ff29a83
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-31 10:31:33 +0900

    ㅇㅇ

--- MH/app.py
+++ MH/app.py
@@ -2,13 +2,8 @@ import streamlit as st
 import sqlite3
 import pandas as pd
 import csv
-
 # 페이지 넓이 설정
 st.set_page_config(page_title='온라인 상점', page_icon=':shopping_bags:', layout='wide')
-
-# 사이드바 설정
-st.sidebar.title('메뉴')
-selected_menu = st.sidebar.radio('', ['상품 구매', '장바구니', '주문 내역'])
 
 # 데이터베이스 연결
 conn = sqlite3.connect(':memory:')
@@ -34,6 +29,7 @@ with open('MH/products.csv', 'r', encoding='utf-8') as f:
 # 커밋
 conn.commit()
 
+
 # 테이블 조회 함수
 def select_products():
     cur.execute("SELECT * FROM products")
@@ -42,31 +38,40 @@ def select_products():
 
 # 테이블 업데이트 함수
 def update_product_quantity(id, quantity):
-    if quantity <= 0:
-        quantity = 0
-        cur.execute("UPDATE products SET quantity = ?, status = '품절' WHERE id = ?", (quantity, id))
-    else:
-        cur.execute("UPDATE products SET quantity = ? WHERE id = ?", (quantity, id))
+    cur.execute("UPDATE products SET quantity = ? WHERE id = ?", (quantity, id))
     conn.commit()
 
 # 상품 정보 표시
-def display_product_info(product, session):
-    col1, col2, col3, col4, col5 = st.beta_columns([1, 1, 1, 0.5, 1])
+def display_product_info(product):
+    col1, col2, col3, col4 = st.beta_columns([1, 1, 1, 0.5])
     with col1:
         st.write(product[0])
     with col2:
         st.write(product[1])
     with col3:
         st.write(product[2])
+        st.write(f"남은 수량: {product[3]}개")  # 수정된 부분
     with col4:
         if product[3] == 0:
             st.write('품절')
         else:
-            quantity = st.number_input('수량', value=1, min_value=1, max_value=product[3])
-            st.write(f'남은 수량: {product[3]-quantity}')
-    with col5:
-        if product[3] == 0:
-            st.write('품절')
-        else:
+            quantity = st.number_input('수량', value=1, min_value=1, max_value=product[3], key=f'quantity_{product[0]}')
             if st.button(f'구매 ({product[1]})', key=f'buy_{product[0]}'):
-                st.write()
+                update_product_quantity(product[0], product[3]-quantity)
+                st.success(f'{product[1]} {quantity}개 구매 완료')
+
+# 메인 함수
+def main():
+    # 상품 정보 가져오기
+    products = select_products()
+
+    # 페이지 헤더
+    st.header('온라인 상점')
+
+    # 페이지 내용
+    for product in products:
+        display_product_info(product)
+
+
+if __name__ == '__main__':
+    main()
