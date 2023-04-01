9de762d2f33c59f782d2252fef09b7f00ea86ac2
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-01 14:35:08 +0900

    dd

--- MH/demo.py
+++ MH/demo.py
@@ -284,13 +284,17 @@ elif choice == "길드페이지":
                     file_name = st.text_input("저장할 파일명을 입력하세요:", "아기자기.xlsx")
                     st.markdown(download_xlsx(data, file_name), unsafe_allow_html=True)
             elif option == "데이터 삭제":
+                de_name = row_index >= 0 and row_index < data.shape[0]
                 # 데이터 삭제 기능
                 if st.button('Delete Data'):
                     # 사용자로부터 삭제할 행 번호 입력받기
                     row_index = st.number_input('삭제하고 싶은 데이터의 번호를 입력해주세요', min_value=0, max_value=data.shape[0]-1)
                 # 해당 행이 존재할 경우, 행을 삭제
+                
                     if row_index >= 0 and row_index < data.shape[0]:
-                        if row_index is True:
+                        if not de_name:
+                            st.warning('입력하신값이 없습니다.')
+                        else : 
                             delete_data(row_index)
                             save_data(data)  # 데이터를 파일에 저장
                             st.success('입력하신 행이 삭제되었습니다.')
