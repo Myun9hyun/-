f96367e7e3e952f4b2ad8eba6c6b287ef3220265
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-02 00:58:03 +0900

    dd

--- MH/practice.py
+++ MH/practice.py
@@ -290,9 +290,8 @@ def main():
             password_input = st.number_input('비밀번호를 입력해주세요 : ')
             if password_input == day_password:
                 st.success('다락방의 낮을 공개할게!')
-                img_url1='https://github.com/Myun9hyun/trash/room/raw/main/MH/image/day.jpg'
-                img_url2='https://github.com/Myun9hyun/trash/room/raw/main/MH/image/day_secret.jpg'
-
+                img_url1='https://github.dev/Myun9hyun/trash/blob/main/MH/room/day.jpg'
+                img_url2='https://github.dev/Myun9hyun/trash/blob/main/MH/room/day_secret.jpg'
                 st.image(img_url1)
                 st.image(img_url2)
         elif option == '아기자기 다락방🌙':
