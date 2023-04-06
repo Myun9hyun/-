286575be66a99d128e0618f96c7921472b51ff2a
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-06 16:45:42 +0900

    ㅇㅇ

--- MH/app.py
+++ MH/app.py
@@ -1,17 +1,12 @@
+# app.py
+
 import streamlit as st
 
-css = """
-h1 {
-  color: blue;
-  font-size: 36px;
-}
+# 커스텀 테마 적용
+st.set_theme({'primaryColor': '#3f51b5', 'backgroundColor': '#f2f2f2',
+              'secondaryBackgroundColor': '#fff', 'textColor': '#333',
+              'font': 'sans-serif'}, "custom_theme")
 
-p {
-  color: red;
-  font-size: 18px;
-}
-"""
-
-st.write(f"<style>{css}</style>", unsafe_allow_html=True)
-st.write("<h1>Hello, World!</h1>", unsafe_allow_html=True)
-st.write("<p>This is a paragraph.</p>", unsafe_allow_html=True)
+# 텍스트 출력
+st.header("This is a header")
+st.write("This is some text.")
