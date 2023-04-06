e8fbd6d2f2f9f7ee7c7c9441ce4bcfb793b394a6
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-06 16:03:30 +0900

    ㅇㅇ

--- MH/app.py
+++ MH/app.py
@@ -4,8 +4,8 @@ import streamlit as st
 
 # 테마 설정
 [theme]
-base="dark"
-primaryColor="purple"
-
-# 앱 내용
-st.write("앱 내용")
+primaryColor = "#E694FF"
+backgroundColor = "#00172B"
+secondaryBackgroundColor = "#0083B8"
+textColor = "#C6CDD4"
+font = "sans-serif"
