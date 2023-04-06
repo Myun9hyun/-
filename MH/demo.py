9120234c5fe18da74d24b6f00d25cfc087009d26
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-06 14:58:06 +0900

    ㅇㅇ

--- MH/demo.py
+++ MH/demo.py
@@ -24,7 +24,7 @@ with st.sidebar:
         "nav-link-selected": {"background-color": "#08c7b4"},
     }
     )
-    st.markdown('<a href="https://github.com/Myun9hyun"><img src="[![GitHub](https://badgen.net/badge/icon/github?icon=github&label)](https://github.com/Myun9hyun)"></a>', unsafe_allow_html=True)
+    st.markdown('<a href="https://github.com/Myun9hyun"><img src="https://badgen.net/badge/icon/github?icon=github&label"></a>', unsafe_allow_html=True)
 
 if choice == "페이지1":
 
