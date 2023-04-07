98db60f0a0a0241e765568c0ff32d7f147671391
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 16:17:21 +0900

    ㅇㅇ

--- MH/app.py
+++ MH/app.py
@@ -8,7 +8,13 @@ image = Image.open("MH/image/newjeans.jpg")
 draw = ImageDraw.Draw(image)
 text = "Hello, world!"
 font = ImageFont.truetype("MH/font/arial-cufonfonts/ARIAL.TTF", 36)
-draw.text((0, 10), text, font=font, fill=(7, 7, 7))
+text_width, text_height = draw.textsize(text, font=font)
+x = (width - text_width) // 2
+y = (height - text_height) // 2
+
+# 이미지에 텍스트 추가
+draw = ImageDraw.Draw(image)
+draw.text((x, y), text, font=font, fill=(255, 255, 255))
 
 # streamlit에 이미지 표시
 st.image(image, caption='Image overlaid with text')
