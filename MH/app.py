ab86f322e5eca02e8323068531367ffd57c14a35
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 09:45:18 +0900

    dd

--- MH/app.py
+++ MH/app.py
@@ -1,15 +1,3 @@
 import streamlit as st
 
-bg_color = "#F0F2F6"
-bg_image = "MH/image/newjeans.jpg"
-
-st.set_page_config(
-    page_title="My Streamlit App",
-    page_icon=":sunglasses:",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    # background 인자
-    background=bg_color,
-    # background_image 인자
-    background_image=bg_image,
-)
+st.write("Streamlit version:", st.__version__)
