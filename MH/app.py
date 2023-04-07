3e0761aead058120160f2a30d9ed9229ca1533a6
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 10:16:51 +0900

    ㅇㅇ

--- MH/app.py
+++ MH/app.py
@@ -1,47 +1,33 @@
+
+
 # import streamlit as st
 # import base64
 
-# st.set_page_config(
-#     page_title="My Streamlit App",
-#     page_icon=":sunglasses:",
-#     layout="wide",
-#     initial_sidebar_state="expanded"
+# with open("MH/image/back.jpg", "rb") as f:
+#     img_bytes = f.read()
+
+# if not img_bytes:
+#     st.write("Error: Failed to read image file.")
+
+# st.write(f"Image Bytes: {img_bytes}")
+
+# b64 = base64.b64encode(img_bytes).decode()
+
+# st.write(f"B64: {b64}")
+
+# st.markdown(
+#     f'<style>body {{background-image: url("data:image/jpeg;base64,{b64}")}}</style>',
+#     unsafe_allow_html=True,
 # )
-
-# with open("MH/image/newjeans.jpg", "rb") as f:
-#     bg_image_data = f.read()
-
-# bg_image_base64 = base64.b64encode(bg_image_data).decode()
-
-# bg_css = f"""
-#     <style>
-#         body {{
-#             background-image: url('data:image/jpg;base64,{bg_image_base64}');
-#             background-size: cover;
-#         }}
-#     </style>
-# """
-
-# st.markdown(bg_css, unsafe_allow_html=True)
-# st.image(bg_image_data)
-# # Streamlit 앱의 나머지 코드
-
+# 
 import streamlit as st
-import base64
 
-with open("MH/image/back.jpg", "rb") as f:
-    img_bytes = f.read()
-
-if not img_bytes:
-    st.write("Error: Failed to read image file.")
-
-st.write(f"Image Bytes: {img_bytes}")
-
-b64 = base64.b64encode(img_bytes).decode()
-
-st.write(f"B64: {b64}")
-
-st.markdown(
-    f'<style>body {{background-image: url("data:image/jpeg;base64,{b64}")}}</style>',
-    unsafe_allow_html=True,
+st.set_page_config(
+    page_title="My Streamlit App",
+    page_icon=":sunglasses:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    page_bg_color="#02ab21"
 )
+
+# Streamlit 앱의 나머지 코드
