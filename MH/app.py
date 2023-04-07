34b0ca34f0e3b5d24b7040d9c42dc019a7013146
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 09:48:07 +0900

    dd

--- MH/app.py
+++ MH/app.py
@@ -1,14 +1,22 @@
 import streamlit as st
 
-bg_image_path = "MH/image/newjeans.jpg"
-
 st.set_page_config(
     page_title="My Streamlit App",
     page_icon=":sunglasses:",
     layout="wide",
-    initial_sidebar_state="expanded",
-    # 배경 이미지 설정
-    page_bg_img=bg_image_path
+    initial_sidebar_state="expanded"
 )
+
+bg_image_url = "MH/image/newjeans.jpg"
+bg_css = f"""
+    <style>
+        body {{
+            background-image: url('{bg_image_url}');
+            background-size: cover;
+        }}
+    </style>
+"""
+
+st.markdown(bg_css, unsafe_allow_html=True)
 
 # Streamlit 앱의 나머지 코드
