645144fb78d22552cb9e6cc63c87a3bd0fbb9b5f
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 09:31:15 +0900

    dd

--- MH/app.py
+++ MH/app.py
@@ -5,6 +5,6 @@ bg_image = 'MH/image/newjeans.jpeg'
 
 # set_page_config 함수를 사용하여 배경을 변경합니다.
 st.set_page_config(layout="wide", page_title='My Streamlit App', page_icon=':smiley:', 
-                   initial_sidebar_state='expanded', background=bg_image)
+                   initial_sidebar_state='expanded', background_image=bg_image)
 
 # 이후에는 일반적인 Streamlit 코드를 작성합니다.
