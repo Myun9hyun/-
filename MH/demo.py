d077cd56e9e7e3e899dfc4947ce88308a3def24f
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 12:14:00 +0900

    dd

--- MH/demo.py
+++ MH/demo.py
@@ -86,7 +86,7 @@ if choice == "페이지1":
         | :---: | :---: | :---: | :---: | :--- |
         | 서상원 |  |  |  |[![GitHub](https://badgen.net/badge/icon/github%20tkd8973?icon=github&label)](https://github.com/tkd8973)|
         | 조성훈 |  |  |  |[![GitHub](https://badgen.net/badge/icon/github%20chohoon901?icon=github&label)](https://github.com/chohoon901)|
-        | 김명현 |  |  |  |[![GitHub](https://badgen.net/badge/icon/github%220Myun9hyun?icon=github&label)](https://github.com/Myun9hyun)|
+        | 김명현 |  |  |  |[![GitHub](https://badgen.net/badge/icon/github%20Myun9hyun?icon=github&label)](https://github.com/Myun9hyun)|
         | 강성욱 |  |  |  |[![GitHub](https://badgen.net/badge/icon/github%20JoySoon?icon=github&label)](https://github.com/JoySoon)|
         ---
         
