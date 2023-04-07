45c81d78204202f36db815c494423e055f972fb4
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 09:38:28 +0900

    sd

--- MH/app.py
+++ MH/app.py
@@ -1,17 +1,7 @@
 import streamlit as st
-import base64
-# 배경에 사용할 이미지 파일 경로를 지정합니다.
-bg_image = 'MH/image/newjeans.jpg'
 
-st.markdown(
-    f"""
-    <style>
-    .reportview-container {{
-        background: url(data:image/jpeg;base64,{base64.b64encode(open(bg_image, "rb").read()).decode()})
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+bg_image = "MH/image/newjeans.jpg"
+st.set_page_config(page_title="My Streamlit App", page_icon=":sunglasses:", layout="wide", initial_sidebar_state="expanded", background_image=bg_image)
 
-# 이후에는 일반적인 Streamlit 코드를 작성합니다.
+# 이미지 출력
+st.image(bg_image, use_column_width=True)
