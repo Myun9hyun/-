5041c0afff6753cc2977ed4f240f4e787ca5b69a
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 16:48:36 +0900

    dd

--- MH/demo.py
+++ MH/demo.py
@@ -29,8 +29,8 @@ width, height = image.size
 # 이미지에 텍스트 추가
 draw = ImageDraw.Draw(image)
 text = "독산 개발진스"
-font = ImageFont.truetype("MH/font/arial-cufonfonts/NanumSquareNeo-eHv.ttf", 50)
-text_width, text_height = draw.textsize(text, font=font)
+font_kor = ImageFont.truetype("MH/font/arial-cufonfonts/NanumSquareNeo-eHv.ttf", 50)
+text_width, text_height = draw.textsize(text, font=font_kor)
 # x = (width - text_width) // 2
 # y = (height - text_height) // 2
 x = (width - text_width) // 2
@@ -38,7 +38,7 @@ y = height - text_height - 20
 
 # 이미지에 텍스트 추가
 draw = ImageDraw.Draw(image)
-draw.text((x, y), text, font=font, fill=(7, 7, 7))
+draw.text((x, y), text, font=font_kor, fill=(7, 7, 7))
 
 # streamlit에 이미지 표시
 st.image(image, use_column_width=True)
