dfdf22c5e05b68c2286a91c2b211efb15aeac565
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 09:46:32 +0900

    dd

--- MH/app.py
+++ MH/app.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
-st.write("Streamlit version:", st.__version__)
+bg_image_path = "MH/image/newjeans.jpg"
+
+st.set_page_config(
+    page_title="My Streamlit App",
+    page_icon=":sunglasses:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    # 배경 이미지 설정
+    page_bg_img=bg_image_path
+)
+
+# Streamlit 앱의 나머지 코드
