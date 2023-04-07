a74385c6a7329765f53bb003edf13b71037fe428
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 22:36:01 +0900

    dd

--- MH/cozem.py
+++ MH/cozem.py
@@ -373,7 +373,7 @@ elif choice == "길드페이지":
                 # else:
                 #     st.warning('비밀번호가 틀렸습니다.')
 
-            if __name__ == '__main__':
+        if __name__ == '__main__':
                 main()
 
 
