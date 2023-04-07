4173266c8b2c4725fbca04ab838a456f9ac1b632
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 15:46:56 +0900

    dd

--- MH/app.py
+++ MH/app.py
@@ -1,5 +1,20 @@
 import streamlit as st
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
-image = Image.open('MH/image/newjeans.jpg')
-st.image(image, caption='뉴진스.')
+# 이미지 열기
+image = Image.open("MH/image/newjeans.jpg")
+
+# 이미지 사이즈 가져오기
+width, height = image.size
+
+# 이미지 위에 텍스트 추가
+draw = ImageDraw.Draw(image)
+text = "이미지와 텍스트 겹쳐서 표시하기"
+font = ImageFont.truetype("arial.ttf", size=30)
+textwidth, textheight = draw.textsize(text, font)
+x = (width - textwidth) / 2
+y = (height - textheight) / 2
+draw.text((x, y), text, font=font, fill=(255, 255, 255, 255))
+
+# 결과 이미지 출력
+st.image(image)
