8218067d5e295a052307d593a5a7ca28deeece4b
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 15:54:09 +0900

    dd

--- MH/app.py
+++ MH/app.py
@@ -10,7 +10,7 @@ width, height = image.size
 # 이미지 위에 텍스트 추가
 draw = ImageDraw.Draw(image)
 text = "hi"
-font = ImageFont.truetype("arial.ttf", 36)  # 한글 폰트 사용
+font = ImageFont.truetype("MH/font/arial.ttf", 36)  # 한글 폰트 사용
 textwidth, textheight = draw.textsize(text, font)
 x = (width - textwidth) / 2
 y = (height - textheight) / 2
