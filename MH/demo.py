81878a8f40ebb266736a0ec525314eca5c03b613
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 16:51:17 +0900

    ee

--- MH/demo.py
+++ MH/demo.py
@@ -42,7 +42,7 @@ z = height - text_height - 100
 # 이미지에 텍스트 추가
 draw = ImageDraw.Draw(image)
 draw.text((x, y), text_kor, font=font_kor, fill=(7, 7, 7))
-draw.text((x, z), text_eng, font=font_eng, fill=(7, 7, 7))
+draw.text((x, z), text_eng, font=font_eng, fill=(255, 255, 255))
 
 # streamlit에 이미지 표시
 st.image(image, use_column_width=True)
