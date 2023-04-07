996eb6be9969f0846214747bdf8263537536e206
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 15:31:49 +0900

    dd

--- MH/app.py
+++ MH/app.py
@@ -1,9 +1,5 @@
 import streamlit as st
 from PIL import Image
 
-st.sidebar.title("Upload Cover Photo")
-cover_photo = st.sidebar.file_uploader("Choose a PNG or JPG image", type=["png", "jpg"])
-
-if cover_photo is not None:
-    img = Image.open(cover_photo)
-    st.image(img, caption='Uploaded Cover Photo', use_column_width=True)
+header_image = Image.open("MH/image/newjeans.jpg")
+st.beta_set_page_config(page_title='My Streamlit App', page_icon=header_image, layout='wide')
