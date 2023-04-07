4cbb455a87381b9b407cbd0600b93e6d5b958484
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 16:20:52 +0900

    ㅇㅇ

--- MH/app.py
+++ MH/app.py
@@ -15,7 +15,7 @@ y = (height - text_height) // 2
 
 # 이미지에 텍스트 추가
 draw = ImageDraw.Draw(image)
-draw.text((x, y), text, font=font, fill=(255, 255, 255))
+draw.text((x, y), text, font=font, fill=(7, 7, 7))
 
 # streamlit에 이미지 표시
 st.image(image, caption='Image overlaid with text')
