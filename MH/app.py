67eeb0ebfe17b42e29c93dc5658785bdff31aa49
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 09:17:10 +0900

    ㅇㅇ

--- MH/app.py
+++ MH/app.py
@@ -1,17 +1,15 @@
 import streamlit as st
 
-def main():
-    st.set_page_config(page_title="My Custom Page Title", page_icon=":smiley:", layout="wide")
+import streamlit as st
 
-    # CSS 파일 적용하기
-    st.markdown("""
-        <style>
-            body {
-                color: #FFFFFF;
-                background-color: #2F2F2F;
-            }
-        </style>
-    """, unsafe_allow_html=True)
+def main():
+    st.set_page_config(
+        page_title="My Custom Page Title",
+        page_icon=":smiley:",
+        layout="wide",
+        # 배경색을 검은색으로 설정합니다.
+        background_color="#000000"
+    )
 
     # Streamlit 애플리케이션 내용 작성하기
     st.write("Hello, world!")
