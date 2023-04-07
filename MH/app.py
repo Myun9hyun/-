2663a856a10c05339fd545676773ad8b19d772b6
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 10:08:45 +0900

    ㅇㅇ

--- MH/app.py
+++ MH/app.py
@@ -9,7 +9,31 @@ st.set_page_config(
 )
 
 with open("MH/image/newjeans.jpg", "rb") as f:
+    bg_image_data = f.read()
+
+bg_image_base64 = base64.b64encode(bg_image_data).decode()
+
+bg_css = f"""
+    <style>
+        body {{
+            background-image: url('data:image/jpg;base64,{bg_image_base64}');
+            background-size: cover;
+        }}
+    </style>
+"""
+
+# st.markdown(bg_css, unsafe_allow_html=True)
+# st.image(bg_image_data)
+# # Streamlit 앱의 나머지 코드
+
+import streamlit as st
+import base64
+
+with open("MH/image/newjeans.jpg", "rb") as f:
     img_bytes = f.read()
+
+if not bg_image_data:
+    st.write("Error: Failed to read image file.")
 
 st.write(f"Image Bytes: {img_bytes}")
 
@@ -21,4 +45,3 @@ st.markdown(
     f'<style>body {{background-image: url("data:image/jpeg;base64,{b64}")}}</style>',
     unsafe_allow_html=True,
 )
-
