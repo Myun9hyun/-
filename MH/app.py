48e4218e0c20879a81e5e62f147803764b7d2048
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 16:08:53 +0900

    dd

--- MH/app.py
+++ MH/app.py
@@ -7,7 +7,7 @@ image = Image.open("MH/image/newjeans.jpg")
 # 이미지에 텍스트 추가
 draw = ImageDraw.Draw(image)
 text = "Hello, world!"
-font = ImageFont.truetype("arial.ttf", 36)
+font = ImageFont.truetype("MH/font/arial-cufonfonts/ARIAL.TTF", 36)
 draw.text((10, 10), text, font=font, fill=(255, 255, 255))
 
 # streamlit에 이미지 표시
