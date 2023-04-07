9c29ebbb3b0675c03bf93939112ad56f378217a8
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 09:38:47 +0900

    dd

--- MH/app.py
+++ MH/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 bg_image = "MH/image/newjeans.jpg"
-st.set_page_config(page_title="My Streamlit App", page_icon=":sunglasses:", layout="wide", initial_sidebar_state="expanded", background_image=bg_image)
+st.set_page_config(page_title="My Streamlit App", page_icon=":sunglasses:", layout="wide", initial_sidebar_state="expanded", background=bg_image)
 
 # 이미지 출력
 st.image(bg_image, use_column_width=True)
