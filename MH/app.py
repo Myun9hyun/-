fa91a52e5dee84853494a150000c79480a7d0a27
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 15:45:43 +0900

    ㅇㅇ

--- MH/app.py
+++ MH/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 from PIL import Image
 
-header_image = Image.open("MH/image/newjeans.jpg")
-st.beta_set_page_config(page_title='My Streamlit App', page_icon=header_image, layout='wide')
+image = Image.open('MH/image/newjeans.jpg')
+st.image(image, caption='뉴진스.')
