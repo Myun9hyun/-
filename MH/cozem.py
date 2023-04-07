b3573407e4270262ee619482013c3d3f4edaa9a3
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 22:32:41 +0900

    dd

--- MH/cozem.py
+++ MH/cozem.py
@@ -221,13 +221,13 @@ elif choice == "길드페이지":
 
 
         def main():
-            
-            options = ["데이터 추가➕", "데이터 조회🔎", "데이터 삭제✂", "데이터 초기화💣", "노블 사용⭕제한❌", "위클리 코젬 계산📋", "데이터 다운로드💾"]
-            option = st.selectbox("기능 선택", options)
             st.error('⚠️길드 간부진만 접근할 수 있는 메뉴입니다!⚠️')
             password_input = st.number_input('비밀번호를 입력해주세요 : ')
             if password_input == password:
                 st.success('접근을 허용합니다')
+            options = ["데이터 추가➕", "데이터 조회🔎", "데이터 삭제✂", "데이터 초기화💣", "노블 사용⭕제한❌", "위클리 코젬 계산📋", "데이터 다운로드💾"]
+            option = st.selectbox("기능 선택", options)
+            
 
             # # 사용자로부터 이름과 점수를 입력받는 UI 구성
             # name = st.text_input('이름을 입력하세요')
