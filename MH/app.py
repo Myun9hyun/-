bf3caa38d30d00acc1d76d6276b122056a240840
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 15:50:10 +0900

    ㅇㅇ

--- MH/app.py
+++ MH/app.py
@@ -10,7 +10,7 @@ width, height = image.size
 # 이미지 위에 텍스트 추가
 draw = ImageDraw.Draw(image)
 text = "이미지와 텍스트 겹쳐서 표시하기"
-font = ImageFont.load_default()
+font = ImageFont.truetype("NanumGothic.ttf", 36)  # 한글 폰트 사용
 textwidth, textheight = draw.textsize(text, font)
 x = (width - textwidth) / 2
 y = (height - textheight) / 2
