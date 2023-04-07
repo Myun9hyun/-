064bebbe91f36c8444474fd81eb3306eef5d3634
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 09:32:27 +0900

    ㅇㅇ

--- MH/app.py
+++ MH/app.py
@@ -3,8 +3,15 @@ import streamlit as st
 # 배경에 사용할 이미지 파일 경로를 지정합니다.
 bg_image = 'MH/image/newjeans.jpeg'
 
-# set_page_config 함수를 사용하여 배경을 변경합니다.
-st.set_page_config(layout="wide", page_title='My Streamlit App', page_icon=':smiley:', 
-                   initial_sidebar_state='expanded', background_image=bg_image)
+st.markdown(
+    f"""
+    <style>
+    .reportview-container {{
+        background: url(data:image/jpeg;base64,{base64.b64encode(open(bg_image, "rb").read()).decode()})
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 # 이후에는 일반적인 Streamlit 코드를 작성합니다.
