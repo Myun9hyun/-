bd72afeb5c9293146480f37f906d90b2f37757fe
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 21:43:42 +0900

    ㅇㅇ

--- MH/app.py
+++ MH/app.py
@@ -1,24 +1,27 @@
 import streamlit as st
 from PIL import Image, ImageDraw, ImageFont
 
-
 # 이미지 업로드
 image = Image.open("MH/image/newjeans.jpg")
-width, height = image.size
-# 이미지에 텍스트 추가
-draw = ImageDraw.Draw(image)
-text = "뉴진스"
-font = ImageFont.truetype("MH/font/arial-cufonfonts/NanumSquareNeo-eHv.ttf", 36)
-text_width, text_height = draw.textsize(text, font=font)
-# x = (width - text_width) // 2
-# y = (height - text_height) // 2
-x = (width - text_width) // 2
-y = height - text_height - 10
 
 # 이미지에 텍스트 추가
 draw = ImageDraw.Draw(image)
-draw.text((x, y), text, font=font, fill=(7, 7, 7))
+text = "Hello, world!"
+font = ImageFont.truetype("MH/font/arial-cufonfonts/ARIAL.TTF", 36)
+text_width, text_height = draw.textsize(text, font=font)
+x = (image.width - text_width) // 2
+y = image.height - text_height - 20
+
+# 테두리 색상과 굵기 설정
+stroke_width = 2
+stroke_fill = (0, 0, 0)
+
+# 테두리가 있는 텍스트 그리기
+draw.text((x - stroke_width, y), text, font=font, fill=stroke_fill, stroke_width=stroke_width)
+draw.text((x + stroke_width, y), text, font=font, fill=stroke_fill, stroke_width=stroke_width)
+draw.text((x, y - stroke_width), text, font=font, fill=stroke_fill, stroke_width=stroke_width)
+draw.text((x, y + stroke_width), text, font=font, fill=stroke_fill, stroke_width=stroke_width)
+draw.text((x, y), text, font=font, fill=(255, 255, 255))
 
 # streamlit에 이미지 표시
 st.image(image, caption='Image overlaid with text')
-
