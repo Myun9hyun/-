7a3188e5deded60ba7b6e5c111d20d402478159d
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-07 09:05:26 +0900

    team repo

--- MH/demo.py
+++ MH/demo.py
@@ -48,7 +48,7 @@ with st.sidebar:
     }
     )
     st.write("💾Team repo")
-    st.markdown('<a href="https://github.com/Myun9hyun"><img src="https://badgen.net/badge/icon/github?icon=github&label"></a>', unsafe_allow_html=True)
+    st.markdown('<a href="https://github.com/tkd8973/DL_Project"><img src="https://badgen.net/badge/icon/github?icon=github&label"></a>', unsafe_allow_html=True)
     st.write("💪Team Notion")
     st.markdown('<a href="https://www.notion.so/DL_PROJECT-82b3fdfbde2e4937b0f9463fce66d056"><img src="https://img.shields.io/badge/Notion-Deep%20Learning-lightgrey"></a>', unsafe_allow_html=True)
 
