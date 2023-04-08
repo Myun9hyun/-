59569cf374882414dfa6abd7c92e3a8d821c12a7
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-08 19:52:04 +0900

    ㅇㅇ

--- MH/app.py
+++ MH/app.py
@@ -4,17 +4,17 @@ st.markdown("""
     <style>
         /* 새로운 글꼴을 추가합니다. */
         @font-face {
-          font-family: 'Montserrat';
-          src: url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+          font-family: 'Roboto';
+          src: url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
           font-weight: normal;
           font-style: normal;
         }
 
         /* 기본 글꼴을 새로운 글꼴로 대체합니다. */
         body {
-          font-family: 'Montserrat', sans-serif;
+          font-family: 'Roboto', sans-serif;
         }
     </style>
 """, unsafe_allow_html=True)
 
-st.write("안녕하세요, Streamlit에서 Montserrat 글꼴을 적용하였습니다.")
+st.write("안녕하세요, Streamlit에서 Roboto 글꼴을 적용하였습니다.")
