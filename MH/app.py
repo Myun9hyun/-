2243302e2e9729015ec57ebf2e514024976d4777
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-08 22:45:04 +0900

    ㅇㅇ

--- MH/app.py
+++ MH/app.py
@@ -312,45 +312,30 @@ elif choice == "길드페이지":
                     # 이름과 점수가 입력되면 데이터프레임에 추가
                     if st.button('데이터 추가'):
                         if role == "본캐":
-                            suro_cozem = Suro_cozem(suro)
-                            flag_cozem = Flag_cozem(flag)
-                            cozem_total = suro_cozem + flag_cozem
-                            novel_value = novel_p(weekly_mission, suro, flag)
-                            # 중복 검사
-                            if name in data['Name'].values:
-                                st.warning(f'{name} (은)는 이미 있는 이름이야!')
-                                return
-                            data = data.append({
-                                'Name': name, 
-                                'Weekly_Mission': weekly_mission, 
-                                'Suro': suro, 
-                                'Suro_Cozem': suro_cozem,
-                                'Flag': flag, 
-                                'Flag_Cozem': flag_cozem,
-                                'Cozem_Total': cozem_total,
-                                'Novel': novel_value
-                            }, ignore_index=True)
+                            name = st.text_input('이름을 입력하세요')
+                            weekly_mission = st.number_input('주간미션 점수를 입력하세요', min_value=0, max_value=5)
+                            suro = st.number_input('수로 점수를 입력하세요', min_value=0, max_value=100000)
+                            flag = st.number_input('플래그 점수를 입력하세요', min_value=0, max_value=1000)
+                            add_data(name, weekly_mission ,suro, flag)  # 수정된 add_data 함수를 호출
+                            save_data(data)  # 데이터를 파일에 저장
+                            st.success('데이터가 추가되었습니다.')
                         elif role == "부캐":
                             main_name = st.text_input("Enter the main character's name")
                             if main_name not in data['Name'].values:
                                 st.warning(f"{main_name} is not in the data.")
                                 return
-                            main_data = data[data["Name"] == main_name].iloc[0]
-                            cozem_total = main_data["Cozem_Total"]
-                            novel_value = novel_p(weekly_mission, suro, flag)
-                            data = data.append({
-                                'Name': name, 
-                                'Weekly_Mission': weekly_mission, 
-                                'Suro': suro, 
-                                'Suro_Cozem': 0,
-                                'Flag': flag, 
-                                'Flag_Cozem': 0,
-                                'Cozem_Total': cozem_total,
-                                'Novel': novel_value
-                            }, ignore_index=True)
-                            # add_data(name, weekly_mission ,suro, flag)  # 수정된 add_data 함수를 호출
-                            # save_data(data)  # 데이터를 파일에 저장
-                            # st.success('데이터가 추가되었습니다.')
+                                main_data = data[data["Name"] == main_name].iloc[0]
+                                cozem_total = main_data["Cozem_Total"]
+                                name = st.text_input('이름을 입력하세요')
+                                weekly_mission = st.number_input('주간미션 점수를 입력하세요', min_value=0, max_value=5)
+                                suro = st.number_input('수로 점수를 입력하세요', min_value=0, max_value=100000)
+                                flag = st.number_input('플래그 점수를 입력하세요', min_value=0, max_value=1000)
+                                add_data(name, weekly_mission ,suro, flag)  # 수정된 add_data 함수를 호출
+                                save_data(data)  # 데이터를 파일에 저장
+                                st.success('데이터가 추가되었습니다.')
+                                # add_data(name, weekly_mission ,suro, flag)  # 수정된 add_data 함수를 호출
+                                # save_data(data)  # 데이터를 파일에 저장
+                                # st.success('데이터가 추가되었습니다.')
 
                 elif option == "데이터 조회🔎":
                     # 저장된 데이터
