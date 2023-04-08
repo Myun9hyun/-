d5ed39b931adbff8eaab4f344c75f4e1a006d6cd
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-08 19:50:17 +0900

    ㅇㅇ

--- MH/app.py
+++ MH/app.py
@@ -1,5 +1,20 @@
 import streamlit as st
 
-st.set_page_config(page_title="My Streamlit App", page_icon=":smiley:", layout="wide", initial_sidebar_state="expanded", font="Noto Sans KR")
+st.markdown("""
+    <style>
+        /* 새로운 글꼴을 추가합니다. */
+        @font-face {
+          font-family: 'Nanum Gothic';
+          src: url('https://cdn.jsdelivr.net/font-nanum/1.0/NanumGothic.ttf') format('truetype');
+          font-weight: normal;
+          font-style: normal;
+        }
 
-st.write("Hello, world!")
+        /* 기본 글꼴을 새로운 글꼴로 대체합니다. */
+        body {
+          font-family: 'Nanum Gothic', sans-serif;
+        }
+    </style>
+""", unsafe_allow_html=True)
+
+st.write("안녕하세요, Streamlit에서 글꼴을 변경하였습니다.")
