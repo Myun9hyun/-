b7e8dc5f262ec5242f65d264c9d2f1a78831e4b0
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-08 19:51:16 +0900

    ㅇㅇ

--- MH/app.py
+++ MH/app.py
@@ -4,17 +4,17 @@ st.markdown("""
     <style>
         /* 새로운 글꼴을 추가합니다. */
         @font-face {
-          font-family: 'Nanum Gothic';
-          src: url('https://cdn.jsdelivr.net/font-nanum/1.0/NanumGothic.ttf') format('truetype');
+          font-family: 'Montserrat';
+          src: url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
           font-weight: normal;
           font-style: normal;
         }
 
         /* 기본 글꼴을 새로운 글꼴로 대체합니다. */
         body {
-          font-family: 'Nanum Gothic', sans-serif;
+          font-family: 'Montserrat', sans-serif;
         }
     </style>
 """, unsafe_allow_html=True)
 
-st.write("안녕하세요, Streamlit에서 글꼴을 변경하였습니다.")
+st.write("안녕하세요, Streamlit에서 Montserrat 글꼴을 적용하였습니다.")
