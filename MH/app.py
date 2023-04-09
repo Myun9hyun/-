e3c361d993eb63832bd74e4fc1d61703b384a848
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-09 14:17:08 +0900

    ㅇㅇ

--- MH/app.py
+++ MH/app.py
@@ -406,10 +406,11 @@ elif choice == "길드페이지":
                     if st.button('노블 사용가능 목록 보기'):
                         # 먼슬리 참여 가능자 명단
                         monthly = data[data['Novel'] == 'O'] & data[data['Role'] == '본캐']
-                        result = data[(data['Novel'] == 'O') and (data['Role'] == '본캐')].values.tolist()
                         main_monthly = data[data['Role'] == '본캐']
-                        # monthly_list = monthly['Name'].tolist()
-                        monthly_list = result['Name'].tolist()
+
+                        
+                        monthly_list = monthly['Name'].tolist()
+                        
                         st.write('이번주 노블 사용가능 목록입니다.(먼슬리 참여 가능자)')
                         # st.write(f"사용가능자 :  {monthly_list}.")
                         st.write(f"사용가능자 :  {result}.")
