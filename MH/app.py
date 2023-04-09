2031119804fa13b92904502aed57c37458989c62
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-09 20:04:55 +0900

    dd

--- MH/app.py
+++ MH/app.py
@@ -584,7 +584,7 @@ elif choice == "직위관리":
                     # 데이터 삭제 기능
                     # if st.button('데이터 삭제'):
                         # 사용자로부터 삭제할 행 번호 입력받기
-                        st.write(data1[['Name','warning_count']])
+                        st.write(data1[['Name','Warning']])
                         row_index = st.number_input('삭제하고 싶은 데이터의 번호를 입력해주세요', min_value=0, max_value=data1.shape[0]-1)
                         st.write("Enter를 입력하면 삭제됩니다.")
                         if st.button('데이터 삭제'):
