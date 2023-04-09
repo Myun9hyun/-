4cfc118d9510bcd0139a8fe319d68655918a0978
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-09 20:07:43 +0900

    dd

--- MH/app.py
+++ MH/app.py
@@ -604,7 +604,7 @@ elif choice == "직위관리":
                         st.success("이름이 추가되었습니다.")
                 elif option == '경고횟수 추가/차감':
                     name = st.text_input("경고자 이름을 입력해주세요")
-                    data1.loc[data1['Name']==name, 'Warning'] = warning_count
+                    warning_count = data1.loc[data1['Name']==name, 'Warning']
                     if st.button("경고횟수 추가"):
                         warning_count += 1
                         save_data1(data1)
