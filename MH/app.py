00521d33483a8620ae1fe5a03c9dd68006587e43
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-09 12:37:05 +0900

    dd

--- MH/app.py
+++ MH/app.py
@@ -335,7 +335,7 @@ elif choice == "길드페이지":
                     flag = st.number_input('플래그 점수', min_value=0)
                     add_character_data(name, is_main_character, weekly_mission, suro, flag)
                     if st.button('추가'):
-                        데이터 추가 함수 호출
+                        # 데이터 추가 함수 호출
                         add_character_data(name, character_type, weekly_mission, suro, flag)
                     # add_data_widget()
                     save_data(data)  # 데이터를 파일에 저장
