106bc69da5cd6953e87e94d246c73ed4d5e2be65
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-09 20:17:26 +0900

    ㅇㅇ

--- MH/app.py
+++ MH/app.py
@@ -598,10 +598,10 @@ elif choice == "직위관리":
                 elif option == "데이터 추가➕":
                     name = st.text_input("경고자 이름을 입력해주세요")
                     warning_count = data1.loc[data1['Name']==name, 'Warning'].values[0] if name in data1['Name'].values else 0
-                    if st.button('이름 추가'):
+                    if st.button('경고자 이름 추가'):
                         add_data1(name, warning_count)
                         save_data1(data1)
-                        st.success("이름이 추가되었습니다.")
+                        st.success(f"경고자 {name}이(가) 추가되었습니다.")
                 elif option == '경고횟수 추가/차감':
                     name = st.text_input("경고자 이름을 입력해주세요")
                     filtered_data = data1.loc[data1['Name'] == name, 'Warning']
