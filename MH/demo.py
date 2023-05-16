b26d98cb27b5ce2b459a272db84880f09188a485
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-05-16 14:13:52 +0900

    dd

--- MH/demo.py
+++ MH/demo.py
@@ -21,7 +21,7 @@ password3 = "초초"
 answer3 = "릎샴"
 
 with st.sidebar:
-    choice = option_menu("Menu", ["메인페이지", "길드페이지", "직위관리", "아카이브", "이것저것"],
+    choice = option_menu("Menu", ["메인페이지", "퀴즈", "아깅이소리함", "아카이브", "이것저것"],
                          icons=['house', 'bi bi-emoji-smile', 'bi bi-robot', 'bi bi-palette'],
                          menu_icon="app-indicator", default_index=0,
                          styles={
@@ -31,10 +31,11 @@ with st.sidebar:
         "nav-link-selected": {"background-color": "#02ab21"},
     }
     )
-    
-quiz1_password = st.text_input("1번 문제 오픈을 위한 비밀번호를 입력해주세요!",  key="quiz1_password")
+# ----------------------------------------------------------------------------------------------------------
+# 1번
+quiz1_password = st.text_input("1번 문제 오픈을 위한 비밀번호를 입력해주세요!",  key="quiz1_password", type='password')
 if quiz1_password == password1:
-    quiz1 = st.text_input("우리 길드의 이름은 뭘까?")
+    quiz1 = st.text_input("우리 길드의 이름은 뭘까?(⊙_⊙)？")
     if st.button("정답 확인", key="check_answer_button1"):
         if quiz1 == answer1:
             st.balloons()
@@ -44,9 +45,11 @@ if quiz1_password == password1:
         else:
             st.warning("다시 한번 생각해봐!")
     if st.button("힌트 보기", key="check_hint_button1"):
-            st.write("이건 힌트 줄수가 없어! 잘 생각해봐")
+            st.write("이건 힌트를 줄 수가 없어! 잘 생각해 봐")
 elif quiz1_password != "" and quiz1_password != password1:
     st.warning("비밀번호가 틀렸어!")
+# ----------------------------------------------------------------------------------------------------------
+# 2번
 quiz2_password = st.text_input("2번 문제 오픈을 위한 비밀번호를 입력해주세요!", key = "quiz2_password")
 if quiz2_password == password2:
     quiz2 = st.text_input("이 페이지 누가 만들었을까요?")
@@ -55,13 +58,17 @@ if quiz2_password == password2:
             st.balloons()
             st.success("정답입니다!")
             st.write("[둥둥향]은 조잡한 페이지 만드는 역할을 맡고 있어!")
+            st.write("[둥둥향]은 공지방에서 이벤트 정리글을 공유하는 역할을 하고 있어!")
             st.write("3번 문제의 비밀번호는 '초초'야!")
         else :
             st.error("다시 한번 생각해봐!")
     if st.button("힌트 보기", key = "check_hint_button2"):
         st.write("만든 사람의 직업은 캐논마스터야!")
+        st.write("만든 사람은 공지방에서 이벤트 알림이 역할을 하고 있어!")
 elif quiz2_password != "" and quiz2_password != password2:
     st.error("비밀번호가 틀렸어!")
+# ----------------------------------------------------------------------------------------------------------
+# 3번
 quiz3_password = st.text_input("3번 문제 오픈을 위한 비밀번호를 입력해주세요!", key = "quiz3_password3")
 if quiz3_password == password3:
     quiz3 = st.text_input("이번 메이플 팬페스트 금손 부스에 참석한 간부는 누구일까요?")
@@ -71,9 +78,13 @@ if quiz3_password == password3:
             st.success("정답입니다!")
             st.write("[릎샴]은 이번 팬페스트에 '볼빵빵하우스'라는 부스 담당자로 참석했어!")
             st.write("[릎샴]은 길드에서 포스터, 길드규정문 등을 만드는 디자인 역할을 하고 있어!")
+            st.write("4번 문제의 비밀번호는 ()야!")
         else : 
             st.warning("다시 생각해봐!")
     if st.button("힌트 보기"):
         st.write("캡틴 김수호와 직업이 같은 사람을 생각해봐!")
 elif quiz3_password != "" and quiz3_password != password3:
     st.error("비밀번호가 틀렸어!")
+# ----------------------------------------------------------------------------------------------------------
+# 4번
+quiz4_password = st.text_input("4번 문제 오픈을 위한 비밀번호를 입력해주세요!")
