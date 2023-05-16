502ae0a0dfd2fe9453c22456157a26620652882f
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-05-16 10:34:30 +0900

    ㅇㅇ

--- MH/demo.py
+++ MH/demo.py
@@ -30,10 +30,13 @@ else :
 quiz2_password = st.text_input("2번 문제 오픈을 위한 비밀번호를 입력해주세요!", key = "quiz2_password")
 if quiz2_password == password2:
     quiz2 = st.text_input("이 페이지 누가 만들었을까요?")
-    if st.button("정답 확인하기"):
+    if st.button("정답 확인"):
         if quiz2 == answer2:
             st.success("정답입니다!")
         else :
             st.error("다시 한번 생각해봐!")
+    if st.button("힌트"):
+        st.write("만든 사람의 직업은 캐논마스터야!")
+
 else :
     st.error("비밀번호가 틀렸어!")
